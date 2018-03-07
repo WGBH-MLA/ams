bec6396e79eb0f74e4c9413e4c64700aaca3c0d6
Adeel Ahmad <adeel.ahmad@geekschicago.com>
2018-03-07 20:55:26 +0500

    AMS2-79 test case for unique adminset

--- app/validators/hyrax/has_one_title_validator.rb
+++ app/validators/hyrax/has_one_title_validator.rb
@@ -4,10 +4,10 @@ module Hyrax
     def validate(record)
       # validates that the title it is unique
       if record.new_record? && AdminSet.where(title: record.title[0]).present?
-        return record.errors[:title] << "Administrative set #{record.title[0]} already exist try another one"
+        return record.errors[:title] << "#{I18n.t('hyrax.dashboard.admin_sets.admin_set')} #{record.title[0]} #{I18n.t('hyrax.dashboard.admin_sets.title_unique_validation')}"
         # validates that the title has at least one title
       elsif record.title.reject(&:empty?).empty?
-        return record.errors[:title] << "You must provide a title"
+        return record.errors[:title] << I18n.t('hyrax.dashboard.admin_sets.title_cant_blank_validation')
       end
     end
   end
