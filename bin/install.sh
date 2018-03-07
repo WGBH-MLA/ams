31af62c05621bdd709ff623a5c242c9cba4425c9
Adeel Ahmad <adeel.ahmad@geekschicago.com>
2018-03-08 01:55:57 +0500

    generate and store rails key in envirement

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
 cd /var/www/ams
 bundle install
 rails db:migrate
 chown -R ec2-user:ec2-user /var/www/ams
+if [ -n $SECRET_KEY_BASE ]; then
+echo "export SECRET_KEY_BASE=`rails secret`" >> /etc/profile
+source /etc/profile
+fi
 service httpd restart
