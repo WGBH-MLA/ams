a95eacd55f9d032a3919496216f4e2a362a172be
Adeel Ahmad <adeel.ahmad@geekschicago.com>
2018-03-08 17:24:22 +0500

    fixing production deployment config

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
 cd /var/www/ams
 bundle install
+if [ -n $RAILS_SECRET_KEY ]; then
+echo "export SECRET_KEY_BASE=`rails secret`" >> /etc/profile
+source /etc/profile
+fi
 rails db:migrate
+rails assets:precompile
 chown -R ec2-user:ec2-user /var/www/ams
 service httpd restart
