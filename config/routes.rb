2837b160263a9606af7e1f7385f75184a22cb7b8
Adeel Ahmad <adeel.ahmad@geekschicago.com>
2018-03-08 21:24:53 +0500

    AMS2-20 adding role management

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,10 @@ Rails.application.routes.draw do
     concerns :searchable
   end
 
+  mount Hydra::RoleManagement::Engine => '/'
+
   devise_for :users
+
   namespace :admin do
     resources :users, only: [:new]
     # Post route for creating new Users
