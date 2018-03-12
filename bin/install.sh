3a1900d8c4cf53af79e16bb29a484f4dd0d8e640
Adeel Ahmad <adeelahmad99@gmail.com>
2018-03-13 00:26:07 +0500

    fixing variable check

--- bin/install.sh
+++ bin/install.sh
@@ -8,7 +8,7 @@ fi
 rails db:migrate
 rails assets:precompile
 chown -R ec2-user:ec2-user /var/www/ams
-if [ -n $SECRET_KEY_BASE ]; then
+if [ -z $SECRET_KEY_BASE ]; then
 echo "export SECRET_KEY_BASE=`rails secret`" >> /etc/profile
 source /etc/profile
 fi
