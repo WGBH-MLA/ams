4251616d0f98035458f1d4509915fa4f6f421658
Adeel Ahmad <adeelahmad99@gmail.com>
2018-03-13 00:31:07 +0500

    removing extra var

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+source /etc/profile
 cd /var/www/ams
 bundle install
-if [ -n $RAILS_SECRET_KEY ]; then
-echo "export SECRET_KEY_BASE=`rails secret`" >> /etc/profile
-source /etc/profile
-fi
 rails db:migrate
 rails assets:precompile
 chown -R ec2-user:ec2-user /var/www/ams
