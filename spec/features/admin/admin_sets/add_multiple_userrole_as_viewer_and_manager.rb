62ea1512b281387d90cfd7d030a023901a76e091
Adeel Ahmad <adeelahmad99@gmail.com>
2018-03-20 01:03:41 +0500

    feature test adding user role as manager and viewer to adminset (#33)

---
+++ spec/features/admin/admin_sets/add_multiple_userrole_as_viewer_and_manager.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+include Warden::Test::Helpers
+
+RSpec.feature 'AssignUserroleAsViewerAndManager.', js: true do
+  context 'Add permissions to user (Role)' do
+    let(:admin_user) { create :admin_user }
+    let!(:user) { create :user }
+    let!(:user_with_role) { create :user_with_role, role_name: 'user' }
+
+    let!(:admin_set_1) { create :admin_set, title: ["Test Admin Set 1"] }
+
+    let!(:admin_set_2) { create :admin_set, title: ["Test Admin Set 2"] }
+    let!(:work_2) { create :public_work, title: ['Second work'] }
+    let!(:permission_template_2) { Hyrax::PermissionTemplate.find_or_create_by!(admin_set_id: admin_set_2.id) }
+    let!(:workflow) { Sipity::Workflow.create!(active: true, name: 'test-workflow', permission_template: permission_template_2) }
+
+    scenario 'Assigning Permissions to AdminSets' do
+      work_2.admin_set_id = admin_set_2.id
+      work_2.save!
+      Hyrax::PermissionTemplateAccess.create!(
+          permission_template_id: permission_template_2.id,
+          agent_type: 'group',
+          agent_id: 'user',
+          access: 'view'
+      )
+
+      login_as(admin_user)
+
+      # Edit first AdminSet
+      visit "/admin/admin_sets/#{admin_set_1.id}/edit"
+      expect(page).to have_content 'Edit Administrative Set'
+
+      # Add user as manager
+      click_on('Participants')
+      fill_in('permission_template_access_grants_attributes_0_agent_id', with: user_with_role.roles[0].name)
+      find("#group-participants-form select option[value='manage']").select_option
+      find('#group-participants-form .btn').click
+      expect(page).to have_content  'participant rights have been updated'
+      logout(admin_user)
+
+      # Login role user to check permissions
+      login_as(user_with_role)
+
+      # Check first AdminSet edit permissions
+      visit "/admin/admin_sets/#{admin_set_1.id}"
+      expect(page).to have_content admin_set_1.title[0]
+      click_on('Edit')
+      expect(page).to have_content 'Edit Administrative Set'
+
+      # Check second AdminSet records in search results
+      visit '/'
+      find("#search-submit-header").click
+      expect(page).to have_content work_2.title[0]
+
+      # open record in search result check it dont have other records edit permissions
+      click_on(work_2.title[0])
+      expect(page).not_to have_content 'Edit'
+
+      exit
+    end
+  end
+
+end
