ecf432f1fb2b0012ed3c75eee78f9cd007306f4f
Andrew Myers <afredmyers@gmail.com>
2018-04-05 09:16:06 -0600

    WIP - Create Series work type
    * Creates a Series work type from `rails g hyrax:work Series`.
    * Modifies Series model, SeriesForm object, Series Presenter object, attribute
      rows partial for Series,
    * Sets required fields to be Title and Description only.
    * Adds PBCore specific fields.
    * Adds ability for everyone to create Series records.
    * Adds test for the SeriesForm object.
    * Adds enhanced integration test for creating new Series.
    
    Also,
    
    * Removes unused code in User factory.
    * Fixes test for creating Asset records.
    * Fixes typo: changes 'Annotiation' to 'Annotation'.
    * Adds a random DateTime helper for more flexible testing on input and
      display of dates.
    * Adds a `wait_for` helper to wait for X seconds for a block to return something
      truthy. This is used to wait X seconds while checking to see if a record
      exists before proceeding in an integration spec, and is better than hardcoding
      a number that may or may not be long enough, or too long.
    * Adds pry-rails for using Pry as Rails console.
    
    Closes AMS2-137.

---
+++ app/actors/hyrax/actors/series_actor.rb
@@ -0,0 +1,8 @@
+# Generated via
+#  `rails generate hyrax:work Series`
+module Hyrax
+  module Actors
+    class SeriesActor < Hyrax::Actors::BaseActor
+    end
+  end
+end
--- app/presenters/hyrax/asset_presenter.rb
+++ app/presenters/hyrax/asset_presenter.rb
@@ -2,6 +2,6 @@
 #  `rails generate hyrax:work Asset`
 module Hyrax
   class AssetPresenter < Hyrax::WorkShowPresenter
-    delegate :broadcast, :created, :copyright_date, :episode_number, :description, :spatial_coverage, :temporal_coverage, :audience_level, :audience_rating, :annotiation, :rights_summary, :rights_link, :date, to: :solr_document
+    delegate :broadcast, :created, :copyright_date, :episode_number, :description, :spatial_coverage, :temporal_coverage, :audience_level, :audience_rating, :annotation, :rights_summary, :rights_link, :date, to: :solr_document
   end
 end
