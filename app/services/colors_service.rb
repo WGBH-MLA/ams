596a1d6af5288713111f8e3d47bfaab102191b15
Adeel Ahmad <adeelahmad99@gmail.com>
2018-04-10 18:42:19 +0500

    Adds Physical Instantiation Work Type

---
+++ app/services/colors_service.rb
@@ -0,0 +1,14 @@
+module ColorsService
+  mattr_accessor :authority
+  self.authority = Qa::Authorities::Local.subauthority_for('colors')
+
+  def self.select_all_options
+    authority.all.map do |element|
+      [element[:label], element[:id]]
+    end
+  end
+
+  def self.label(id)
+    authority.find(id).fetch('term')
+  end
+end
