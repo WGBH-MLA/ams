3580b32c83c11a09d50a5c61f264d945864f2fed
Adeel Ahmad <adeelahmad99@gmail.com>
2018-04-18 20:08:07 +0500

    Removing Work WorkType

--- spec/features/create_asset_spec.rb
+++ spec/features/create_asset_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
+require 'securerandom'
 include Warden::Test::Helpers
 
 RSpec.feature 'Create and Validate Asset', js: true do
   context 'Create adminset, create asset' do
     let(:admin_user) { create :admin_user }
     let!(:user_with_role) { create :user_with_role, role_name: 'user' }
-    let!(:admin_set) { create :admin_set, title: ["Test Admin Set"] }
-
-    let!(:permission_template) { Hyrax::PermissionTemplate.find_or_create_by!(admin_set_id: admin_set.id) }
+    let(:admin_set_id) { AdminSet.find_or_create_default_admin_set_id }
+    let(:permission_template) { Hyrax::PermissionTemplate.find_or_create_by!(admin_set_id: admin_set_id) }
     let!(:workflow) { Sipity::Workflow.create!(active: true, name: 'test-workflow', permission_template: permission_template) }
 
     let(:input_date_format) { '%m/%d/%Y' }
@@ -68,7 +68,7 @@ RSpec.feature 'Create and Validate Asset', js: true do
       fill_in('Rights link', with: asset_attributes[:rights_link])
 
       click_link "Relationships" # define adminset relation
-      find("#asset_admin_set_id option[value='#{admin_set.id}']").select_option
+      find("#asset_admin_set_id option[value='#{admin_set_id}']").select_option
 
       # set it public
       find('body').click
