10532a1158677685a4e2703af1d07be7bc24bed1
Adeel Ahmad <adeelahmad99@gmail.com>
2018-04-18 19:28:30 +0500

    removing extra language field

--- app/forms/hyrax/digital_instantiation_form.rb
+++ app/forms/hyrax/digital_instantiation_form.rb
@@ -3,7 +3,7 @@
 module Hyrax
   class DigitalInstantiationForm < Hyrax::Forms::WorkForm
     self.model_class = ::DigitalInstantiation
-    self.terms += [:date, :dimensions, :digital_format, :standard, :location, :media_type, :generations,:file_size, :time_start, :duration, :data_rate, :colors, :language, :rights_summary, :rights_link, :annotation]
+    self.terms += [:date, :dimensions, :digital_format, :standard, :location, :media_type, :generations,:file_size, :time_start, :duration, :data_rate, :colors, :rights_summary, :rights_link, :annotation]
     self.terms -= [:description, :relative_path, :import_url, :date_created, :resource_type, :creator, :contributor, :keyword, :license, :rights_statement, :publisher, :subject, :identifier, :based_near, :related_url, :bibliographic_citation, :source]
     self.required_fields -= [:creator, :keyword, :rights_statement]
     self.required_fields += [:media_type, :digital_format, :location]
