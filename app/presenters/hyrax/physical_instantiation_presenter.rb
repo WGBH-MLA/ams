511b62d8dd7497d7a8f1ae9b6bd4d1fd3b9c60e2
Jason Corum <jason.corum@gmail.com>
2018-05-03 11:16:58 -0700

    Completes Addition of Instantiation Fields
    * Closes #AMS2-226

--- app/presenters/hyrax/physical_instantiation_presenter.rb
+++ app/presenters/hyrax/physical_instantiation_presenter.rb
@@ -2,6 +2,6 @@
 #  `rails generate hyrax:work PhysicalInstantiation`
 module Hyrax
   class PhysicalInstantiationPresenter < Hyrax::WorkShowPresenter
-    delegate :date, :digitization_date, :dimensions, :format, :standard, :location, :media_type, :generations, :time_start, :duration, :colors, :language, :rights_summary, :rights_link, :annotation, to: :solr_document
+    delegate :date, :digitization_date, :dimensions, :format, :standard, :location, :media_type, :generations, :time_start, :duration, :colors, :language, :rights_summary, :rights_link, :annotation, :local_instantiation_identifer, :tracks, :channel_configuration, :alternative_modes, to: :solr_document
   end
 end
