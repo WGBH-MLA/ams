f71400c9620daf7ee66abd859171416d14bf7439
Adeel Ahmad <adeelahmad99@gmail.com>
2018-05-07 21:11:11 +0500

    cleaning up

--- app/forms/hyrax/digital_instantiation_form.rb
+++ app/forms/hyrax/digital_instantiation_form.rb
@@ -3,8 +3,7 @@
 module Hyrax
   class DigitalInstantiationForm < Hyrax::Forms::WorkForm
     self.model_class = ::DigitalInstantiation
-    self.terms += [:digital_instantiation_pbcore_xml]
-    self.terms += [:date, :dimensions, :digital_format, :standard, :location, :media_type, :generations,:file_size, :time_start, :duration, :data_rate, :colors, :rights_summary, :rights_link, :annotation, :local_instantiation_identifer, :tracks, :channel_configuration, :alternative_modes]
+    self.terms += [:digital_instantiation_pbcore_xml, :date, :dimensions, :digital_format, :standard, :location, :media_type, :generations,:file_size, :time_start, :duration, :data_rate, :colors, :rights_summary, :rights_link, :annotation, :local_instantiation_identifer, :tracks, :channel_configuration, :alternative_modes]
     self.terms -= [:description, :relative_path, :import_url, :date_created, :resource_type, :creator, :contributor, :keyword, :license, :rights_statement, :publisher, :subject, :identifier, :based_near, :related_url, :bibliographic_citation, :source]
     self.required_fields -= [:creator, :keyword, :rights_statement]
     self.required_fields += [:digital_instantiation_pbcore_xml, :location]
