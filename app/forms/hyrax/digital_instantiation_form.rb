059c5199cfbdefc15fad660dd49847edc69b6655
Adeel Ahmad <adeelahmad99@gmail.com>
2018-05-14 19:54:29 +0500

    reorder & group digital instantiation fields

--- app/forms/hyrax/digital_instantiation_form.rb
+++ app/forms/hyrax/digital_instantiation_form.rb
@@ -3,10 +3,17 @@
 module Hyrax
   class DigitalInstantiationForm < Hyrax::Forms::WorkForm
     self.model_class = ::DigitalInstantiation
-    self.terms += [:digital_instantiation_pbcore_xml, :date, :dimensions, :digital_format, :standard, :location, :media_type, :generations,:file_size, :time_start, :duration, :data_rate, :colors, :rights_summary, :rights_link, :annotation, :local_instantiation_identifer, :tracks, :channel_configuration, :alternative_modes]
     self.terms -= [:description, :relative_path, :import_url, :date_created, :resource_type, :creator, :contributor, :keyword, :license, :rights_statement, :publisher, :subject, :identifier, :based_near, :related_url, :bibliographic_citation, :source]
-    self.required_fields -= [:creator, :keyword, :rights_statement]
-    self.required_fields += [:digital_instantiation_pbcore_xml, :location]
+    self.required_fields -= [:title, :creator, :keyword, :rights_statement]
+    self.required_fields += [:digital_instantiation_pbcore_xml]
+
+    class_attribute :technical_info, :identifying_info , :rights
+
+    self.technical_info = [:local_instantiation_identifer, :media_type, :digital_format, :dimensions, :standard, :file_size, :duration, :time_start, :data_rate, :colors, :tracks, :channel_configuration, :alternative_modes]
+    self.identifying_info = [:title, :location, :generations, :language, :date, :annotation]
+    self.rights = [:rights_summary, :rights_statement]
+
+    self.terms += self.required_fields + self.technical_info +  self.identifying_info + self.rights
 
     def self.model_attributes(form_params)
       clean_params = sanitize_params(form_params)
@@ -16,12 +23,5 @@ module Hyrax
       clean_params
     end
 
-    def primary_terms
-      if self.agreement_accepted
-        return self.required_fields -= [:digital_instantiation_pbcore_xml]
-      end
-      self.required_fields
     end
-
-  end
 end
