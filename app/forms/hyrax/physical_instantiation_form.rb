34012662214f5c4a856d4fe3dcfd5a16766e740f
Adeel Ahmad <adeelahmad99@gmail.com>
2018-05-14 20:13:23 +0500

    reorder & group physical instantiation fields

--- app/forms/hyrax/physical_instantiation_form.rb
+++ app/forms/hyrax/physical_instantiation_form.rb
@@ -3,9 +3,18 @@
 module Hyrax
   class PhysicalInstantiationForm < Hyrax::Forms::WorkForm
     self.model_class = ::PhysicalInstantiation
-    self.terms += [:date, :digitization_date, :dimensions, :format, :standard, :location, :media_type, :generations, :time_start, :duration, :colors, :rights_summary, :rights_link, :annotiation, :local_instantiation_identifer, :tracks, :channel_configuration, :alternative_modes]
+
     self.terms -= [:description, :relative_path, :import_url, :date_created, :resource_type, :creator, :contributor, :keyword, :license, :rights_statement, :publisher, :subject, :identifier, :based_near, :related_url, :bibliographic_citation, :source]
-    self.required_fields -= [:creator, :keyword, :rights_statement]
-    self.required_fields += [:media_type, :format, :location]
+    self.required_fields -= [:title, :creator, :keyword, :rights_statement]
+
+    class_attribute :technical_info, :identifying_info , :rights
+
+    self.identifying_info = [:title, :local_instantiation_identifer, :media_type, :format, :location, :generations, :date, :language, :annotiation]
+    self.technical_info = [:dimensions, :standard, :duration, :time_start, :colors, :tracks, :channel_configuration, :alternative_modes]
+    self.rights = [:rights_summary, :rights_statement]
+
+    self.terms += self.required_fields + self.technical_info +  self.identifying_info + self.rights
+
+
   end
 end
