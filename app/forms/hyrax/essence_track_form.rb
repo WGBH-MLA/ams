e5e50964f27dfe1f70d369c33872e7bce2740787
Adeel Ahmad <adeelahmad99@gmail.com>
2018-05-14 20:20:58 +0500

    reordering essense track fields

--- app/forms/hyrax/essence_track_form.rb
+++ app/forms/hyrax/essence_track_form.rb
@@ -3,10 +3,11 @@
 module Hyrax
   class EssenceTrackForm < Hyrax::Forms::WorkForm
     self.model_class = ::EssenceTrack
-    self.terms += [:track_type, :track_id, :standard, :encoding, :data_rate, :frame_rate,:playback_inch_per_sec, :playback_frame_per_sec,
-                   :sample_rate, :bit_depth, :frame_width, :frame_height, :time_start, :duration, :annotation]
     self.terms -= [:description, :relative_path, :import_url, :date_created, :resource_type, :creator, :contributor, :keyword, :license, :rights_statement, :publisher, :subject,
-                   :identifier, :based_near, :related_url, :bibliographic_citation, :source]
+                   :identifier, :based_near, :related_url, :bibliographic_citation, :source, :language]
+
+    self.terms += [:track_id, :track_type, :standard, :encoding, :frame_rate, :data_rate, :playback_inch_per_sec, :playback_frame_per_sec,
+                   :sample_rate, :bit_depth, :language, :aspect_ratio, :frame_width, :frame_height, :duration, :time_start, :annotation]
     self.required_fields -= [:creator, :keyword, :rights_statement]
     self.required_fields += [:track_type, :track_id]
   end
