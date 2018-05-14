74de9fc4cd7c0f67d2d9bb2e61ded68a9258aa5b
Andrew Myers <afredmyers@gmail.com>
2018-05-14 01:02:17 -0600

    Improve styling of typed fields
    Also, removes unused SimpleForm input class.

--- app/input/multiple_titles_with_types_input.rb
+++ app/input/multiple_titles_with_types_input.rb
@@ -5,12 +5,17 @@ class MultipleTitlesWithTypesInput < MultiValueInput
     # rather than being hardcoded here.
     title_type_choices = TitleAndDescriptionTypesService.select_all_options
 
-    select_input_html_options = { name: "#{@builder.object_name}[title_type][]"}
-    text_input_html_options = { name: "#{@builder.object_name}[title_value][]", value: value[1] }
+    select_input_html_options = input_html_options.merge({
+      name: "#{@builder.object_name}[title_type][]"
+    })
 
+    text_input_html_options = input_html_options.merge({
+      name: "#{@builder.object_name}[title_value][]",
+      value: value[1]
+    })
 
-    output = @builder.select(:title_type, title_type_choices, { selected: value[0] }, select_input_html_options)
-    output += @builder.text_field(:title_value, text_input_html_options)
+    output = @builder.text_field(:title_value, text_input_html_options)
+    output += @builder.select(:title_type, title_type_choices, { selected: value[0] }, select_input_html_options)
     output
   end
 
