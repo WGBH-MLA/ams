dde699471e88fc93ffcbda34a9551d669721a2f4
Jason Corum <jason.corum@gmail.com>
2018-05-18 13:42:28 -0700

    Updates hydra-editor + removes method overwrites
    * Updates hydra-editor gem since new version published fixes issue with method we overwrote
    * Closes AMS2-311

--- app/input/multiple_descriptions_with_types_input.rb
+++ app/input/multiple_descriptions_with_types_input.rb
@@ -16,22 +16,4 @@ class MultipleDescriptionsWithTypesInput < MultiValueInput
     output += @builder.select(:description_type, description_type_choices, { selected: value[0] }, select_input_html_options)
     output
   end
-
-
-  # Overrides MultiValueInput#collection from Hydra-editor. The original
-  # method calls object[attribute_name] instead of object.send(attribute_name)
-  # to retrieve the value. By using the square brackets, it bypasses any
-  # accessor method on the form object that you may have created to decorate
-  # the values, which is exactly what we are doing.
-  # TODO: Create a PR for hydra-editor to change `object[attribute_name]` to
-  # `object.send(attribute_name)` in MultiValueInput#collection.
-  def collection
-    @collection ||= begin
-      # As of this writing, the line below is the only once changed from the
-      # original.
-      val = object.send(attribute_name)
-      col = val.respond_to?(:to_ary) ? val.to_ary : val
-      col.reject { |value| value.to_s.strip.blank? } + ['']
-    end
-  end
 end
