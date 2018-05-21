c0746db8ad20c40a1517f83874f3ab4feec9f4b8
Adeel Ahmad <adeelahmad99@gmail.com>
2018-05-21 16:11:40 +0500

    refactoring questionin authority

--- app/services/asset_types_service.rb
+++ app/services/asset_types_service.rb
@@ -1,14 +1,5 @@
-module AssetTypesService
-  mattr_accessor :authority
-  self.authority = Qa::Authorities::Local.subauthority_for('asset_types')
-
-  def self.select_all_options
-    authority.all.map do |element|
-      [element[:label], element[:id]]
-    end
-  end
-
-  def self.label(id)
-    authority.find(id).fetch('term')
+class AssetTypesService < Hyrax::QaSelectService
+  def initialize
+    super('asset_types')
   end
 end
