fdf075e31797d5f5e7b957bdb6c971cdfd5c34a9
Adeel Ahmad <adeelahmad99@gmail.com>
2018-07-03 10:51:05 -0400

    adding basic model tests

---
+++ spec/factories/physical_instantiation.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :physical_instantiation, class: PhysicalInstantiation do
+    id { Noid::Rails::Service.new.mint }
+    sequence(:title) { |n| ["Test Physical Instantiation #{n}"] }
+    location "Test location"
+    format "Test format"
+    media_type "Test media_type"
+  end
+end
