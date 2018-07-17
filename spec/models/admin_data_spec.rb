fa959a90ead286445d3a2d06c4eb181a753eaf3a
Adeel Ahmad <adeelahmad99@gmail.com>
2018-07-18 02:00:31 +0500

    Adding rspec tests

--- spec/models/admin_data_spec.rb
+++ spec/models/admin_data_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe AdminData, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  it "has level_of_user_access"
+  it "has minimally_cataloged"
+  it "has outside_url"
+  it "has special_collection"
+  it "has transcript_status"
+  it "has sonnyci_id"
+  it "has licensing_info"
+
+  context "attributes" do
+    let(:admin_data) { FactoryBot.build(:admin_data) }
+    it "is valid with valid attributes" do
+      expect(admin_data.valid?).to be true
+    end
+  end
+
+  context "gid" do
+    let(:admin_data) { FactoryBot.create(:admin_data) }
+    it "has gid when created" do
+      expect(admin_data.valid?).to be true
+    end
+  end
 end
