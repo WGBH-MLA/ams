8187f2ca160c1e677fcf598bbd85a58e37f42073
Adeel Ahmad <adeelahmad99@gmail.com>
2018-07-24 23:37:01 +0500

    Merge branch develop into AMS2-195_AMS2-403_AMS2-404_adds_facets

--- app/indexers/asset_indexer.rb
+++ app/indexers/asset_indexer.rb
@@ -9,19 +9,20 @@ class AssetIndexer < Hyrax::WorkIndexer
   # this behavior
   include Hyrax::IndexesLinkedMetadata
 
+  self.thumbnail_path_service = WGBH::WorkThumbnailPathService
+
   def generate_solr_document
-   if object.admin_data
-    super.tap do |solr_doc|
-     solr_doc['admin_data_tesim'] = object.admin_data.gid
-     solr_doc['level_of_user_access_tesim'] = object.admin_data.level_of_user_access
-     solr_doc['minimally_cataloged_tesim'] = object.admin_data.minimally_cataloged.to_s
-     solr_doc['outside_url_tesim'] = object.admin_data.outside_url
-     solr_doc['special_collection_tesim'] = object.admin_data.special_collection
-     solr_doc['transcript_status_tesim'] = object.admin_data.transcript_status
-     solr_doc['sonyci_id_tesim'] = object.admin_data.sonyci_id
-     solr_doc['licensing_info_tesim'] = object.admin_data.licensing_info
+    if object.admin_data
+      super.tap do |solr_doc|
+        solr_doc['admin_data_tesim'] = object.admin_data.gid
+        solr_doc['level_of_user_access_tesim'] = object.admin_data.level_of_user_access
+        solr_doc['minimally_cataloged_tesim'] = object.admin_data.minimally_cataloged.to_s
+        solr_doc['outside_url_tesim'] = object.admin_data.outside_url
+        solr_doc['special_collection_tesim'] = object.admin_data.special_collection
+        solr_doc['transcript_status_tesim'] = object.admin_data.transcript_status
+        solr_doc['sonyci_id_tesim'] = object.admin_data.sonyci_id
+        solr_doc['licensing_info_tesim'] = object.admin_data.licensing_info
+      end
     end
-   end
   end
 end
-
--- spec/factories/hyrax_factories.rb
+++ spec/factories/hyrax_factories.rb
@@ -2,6 +2,13 @@
 hyrax_factories = [
   'admin_sets',
   'permission_templates',
+  'permission_template_accesses',
+  'collections',
+  'collections_factory',
+  'collection_types',
+  'collection_type_participants',
+  'collection_branding_infos',
+  'object_id',
   'workflows',
   'workflow_actions'
 ]
