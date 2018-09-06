6d5f39777609d41c746c477d5ec07621200cc39a
Adeel Ahmad <adeelahmad99@gmail.com>
2018-09-06 14:41:48 +0500

    adding series title and description and refactoring code

--- app/input/multiple_dates_with_types_input.rb
+++ app/input/multiple_dates_with_types_input.rb
@@ -1,28 +1,22 @@
-class MultipleDatesWithTypesInput < MultiValueInput
+class MultipleDatesWithTypesInput < AMS::MultiTypedInput
 
-  def build_field(value, index)
+  def text_input_html_options(value, index)
+    options = super
+    options[:pattern] = AMS::NonExactDateService.regex.to_s
+    options
+  end
+
+  def type_choices
     date_types_service = DateTypesService.new
-    date_type_choices = date_types_service.select_all_options
+    choices = date_types_service.select_all_options
+  end
 
-    input_dom_id_prefix = "#{object_name}_#{attribute_name}_#{index}"
+  def fields_prefix
+    "date"
+  end
 
-    select_input_html_options = input_html_options.dup.merge({
-      name: "#{@builder.object_name}[date_type][]",
-      id: input_dom_id_prefix + "_type"
-    })
-
-    date_input_html_options = input_html_options.dup.merge({
-      name: "#{@builder.object_name}[date_value][]",
-      value: value[1],
-      autocomplete:"off",
-      id: input_dom_id_prefix + "_input"
-    })
-
-    date_input_html_options[:class] += ["datepicker","multi_value","multi-text-field"]
-    date_input_html_options[:pattern] = AMS::NonExactDateService.regex.to_s
-
-    output = @builder.text_field(:date_value, date_input_html_options)
-    output += @builder.select(:date_type, date_type_choices, { selected: value[0] }, select_input_html_options)
-    output
+  def input_css_classes
+    css_classes = super
+    css_classes << "datepicker"
   end
 end
