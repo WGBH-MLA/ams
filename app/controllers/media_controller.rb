2b0cf57ff3a1c8fc8627ca37b5a98a531c997fa0
Andrew Myers <andrew_myers@wgbh.org>
2018-11-19 13:02:03 -0500

    Add tests for SonyCi media urls
    * Refactors MediaController to...
      * Memoize solr_document.
      * Check ability to :show solr_document.
      * Returns 404 if solr_document not found.
      * Returns 403 if unauthorized.
    * Adds controller spec for MediaController.
    * Adds routing spec for urls of the form /media[id].

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,18 +1,31 @@
 require 'sony_ci_api'
 
 class MediaController < ApplicationController
-  before_action :set_solr_document
-
   def show
-    ci = SonyCiBasic.new(credentials_path: Rails.root + 'config/ci.yml')
-    redirect_to ci.download(@solr_document['sonyci_id_tesim'][(params['part'] || 0).to_i])
+    if solr_document
+      if can? :show, solr_document
+        redirect_to download_url
+      else
+        head :forbidden
+      end
+    else
+      head :not_found
+    end
   end
 
   private
 
-  def set_solr_document
-    @solr_document = ActiveFedora::Base.find(params['id']).to_solr
-    raise "Unable to find SolrDocument with ID=#{params['id']}" if @solr_document.nil?
-    @solr_document
-  end
+    def download_url
+      @download_url ||= ci.download(solr_document['sonyci_id_tesim'][(params['part'] || 0).to_i])
+    end
+
+    def ci
+      @ci ||= SonyCiBasic.new(credentials_path: Rails.root + 'config/ci.yml')
+    end
+
+    def solr_document
+      @solr_document ||= ActiveFedora::Base.find(params['id']).to_solr
+    rescue ActiveFedora::ObjectNotFoundError
+      nil
+    end
 end
