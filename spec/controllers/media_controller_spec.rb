57e802f0f4d4f9994c936176cc92bde0ed163451
Andrew Myers <andrew_myers@wgbh.org>
2018-11-19 13:02:03 -0500

    Add tests for SonyCi media urls
    * Refactors MediaController to...
      * Memoize solr_document.
      * Check ability to :show solr_document.
      * Returns 404 if solr_document not found.
      * Returns 403 if unauthorized.
    * Adds controller spec for MediaController.
    * Adds routing spec for urls of the form /media[id].

---
+++ spec/controllers/media_controller_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe MediaController, type: :controller do
+  describe 'GET #show' do
+    let(:id) { '1234' }
+    let(:sony_ci_id) { '4567' }
+    let(:fake_sony_ci_url) { "https://fakesonyci.com/download/#{sony_ci_id}"}
+    let(:fake_sony_ci_api) { instance_double("SonyCiBasic") }
+    let(:fake_solr_document) { { 'sonyci_id_tesim' => [sony_ci_id] } }
+
+    before do
+      allow(fake_sony_ci_api).to receive(:download).with(sony_ci_id).and_return(fake_sony_ci_url)
+      allow(controller).to receive(:ci).and_return(fake_sony_ci_api)
+      allow(controller).to receive(:solr_document).and_return(fake_solr_document)
+      allow(controller).to receive(:can?).with(:show, fake_solr_document).and_return(true)
+    end
+
+    context 'when no Solr document is found for the given :id' do
+      # Pretend like the controller failed to find the Solr document.
+      before { allow(controller).to receive(:solr_document).and_return(nil) }
+      it 'a 404 HTTP status is returned' do
+        get :show, params: { id: id }
+        expect(response).to have_http_status 404
+      end
+    end
+
+    context 'when a Solr document is found for the given :id' do
+      context 'and when user has permission to view the file' do
+        it 'the SonyCiApi is used to fetch the media url' do
+          get :show, params: { id: id }
+          expect(fake_sony_ci_api).to have_received(:download).with(sony_ci_id)
+          expect(response).to redirect_to fake_sony_ci_url
+        end
+      end
+
+      context 'and when user does NOT have permission to view the file' do
+        # Pretend the user doesn't have permisison to view the Solr document
+        before { allow(controller).to receive(:can?).with(:show, fake_solr_document).and_return(false) }
+        it 'a 403 HTTP status is returned' do
+          get :show, params: { id: id }
+          expect(response).to have_http_status 403
+        end
+      end
+    end
+  end
+end
