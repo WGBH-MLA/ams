4c1db5ec940e9640999e89630e6a58ae3afb7e1a
Andrew Myers <andrew_myers@wgbh.org>
2018-11-25 15:24:43 -0500

    Uses shared specs from batch ingest
    * Adds shared specs from hyrax-batch_ingest gem to test ZippedPBCoreReader class.
    
    Also...
    
    * DRY up spec for ZippedPBCoreReader a little.
    * Fix FactoryBot deprecation warnings re: static attributes.
    * Removes permisstion_templates.rb factory (nothing in there).

--- spec/factories/physical_instantiation.rb
+++ spec/factories/physical_instantiation.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :physical_instantiation, class: PhysicalInstantiation do
     id { Noid::Rails::Service.new.mint }
     sequence(:title) { |n| ["Test Physical Instantiation #{n}"] }
-    location "Test location"
-    format "Test format"
-    media_type "Test media_type"
+    location { "Test location" }
+    format { "Test format" }
+    media_type { "Test media_type" }
   end
 end
