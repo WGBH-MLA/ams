165fe9c05360a59e8cafc70f2dfc1f60a9f30c6d
Jason Corum <jason.corum@gmail.com>
2019-01-08 17:21:27 -0800

    Adds download button for Assets with AAPB Proxies

--- spec/factories/digital_instantiation.rb
+++ spec/factories/digital_instantiation.rb
@@ -5,10 +5,16 @@ FactoryBot.define do
     location { "Test location" }
     digital_format { "Test digital_format" }
     media_type { "Test media_type" }
+    generations { [ "Proxy"] }
     digital_instantiation_pbcore_xml { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/sample_instantiation_valid.xml'), 'text/xml') }
     trait :aapb_moving_image do
       holding_organization { "American Archive of Public Broadcasting" }
       media_type { "Moving Image" }
+    end
+    trait :aapb_moving_image_with_essence_track do
+      holding_organization { "American Archive of Public Broadcasting" }
+      media_type { "Moving Image" }
+      members { [ create(:essence_track)] }
     end
     trait :aapb_sound do
       holding_organization { "American Archive of Public Broadcasting" }
