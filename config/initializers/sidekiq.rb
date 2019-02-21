bd67e0b2239449ccf57beec3c42b41bf58a303c6
Andrew Myers <andrew_myers@wgbh.org>
2019-02-21 15:25:14 -0500

    Alter some production configs
    * Increase net timeout for Sidekiq conn to Redis.
    * Sets Sidekiq logging to verbose.
    * Sets Sidekiq concurrency to 10.
    * Sets production db pool to 20.
    
    These are in response to several multiple errors resulting in Sidekiq jobs
    erroring.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 config = YAML.load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
 
-redis_conn = { url: "redis://#{config[:host]}:#{config[:port]}/" }
+redis_conn = { url: "redis://#{config[:host]}:#{config[:port]}/", network_timeout: config[:network_timeout] }
 
 Sidekiq.configure_server do |s|
   s.redis = redis_conn
