c8575a5c494472489272b5429a871650138323b3
Andrew Myers <andrew_myers@wgbh.org>
2019-02-25 14:34:10 -0500

    Uses REDIS_SERVER for redis host
    In production environments, the Redis server is set in an env var. Use that first
    and default to localhost if it's not there.

--- spec/features/admin/admin_sets/add_userrole_as_viewer_spec.rb
+++ spec/features/admin/admin_sets/add_userrole_as_viewer_spec.rb
@@ -40,6 +40,7 @@ RSpec.feature 'AssignRoleViewer.', js: true do
 
       # Check other user viewer search permissions exist
       visit '/admin/admin_sets'
+      sleep 5
       expect(page).to have_content 'You are not authorized to access this page.'
     end
   end
