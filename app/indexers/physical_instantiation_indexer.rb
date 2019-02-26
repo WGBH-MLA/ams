8fe87959e2a6c0b00c585ae7dc08a91ecda5d248
Adeel Ahmad <adeelahmad99@gmail.com>
2019-02-26 22:15:06 +0500

    Adding views for Instantiation Admin Data

--- app/indexers/physical_instantiation_indexer.rb
+++ app/indexers/physical_instantiation_indexer.rb
@@ -11,10 +11,14 @@ class PhysicalInstantiationIndexer < AMS::WorkIndexer
 
   self.thumbnail_path_service = AAPB::WorkThumbnailPathService
 
-  # Uncomment this block if you want to add custom indexing behavior:
-  # def generate_solr_document
-  #  super.tap do |solr_doc|
-  #    solr_doc['my_custom_field_ssim'] = object.my_custom_property
-  #  end
-  # end
+  def generate_solr_document
+    super.tap do |solr_doc|
+      if object.instantiation_admin_data
+        #Indexing as english text so we can use it on asset show page
+        solr_doc['instantiation_admin_data_tesim'] = object.instantiation_admin_data.gid if !object.instantiation_admin_data.gid.blank?
+        solr_doc['aapb_preservation_lto_ssim'] = solr_doc['aapb_preservation_lto_tesim'] = object.instantiation_admin_data.aapb_preservation_lto if !object.instantiation_admin_data.aapb_preservation_lto.blank?
+        solr_doc['aapb_preservation_disk_ssim'] = solr_doc['aapb_preservation_disk_tesim'] = object.instantiation_admin_data.aapb_preservation_disk if !object.instantiation_admin_data.aapb_preservation_disk.blank?
+      end
+    end
+  end
 end
