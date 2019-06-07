fdda0256966495b6facb7a52ed6bb5247c9629eb
foglabs <foglaboratories@gmail.com>
2019-06-07 15:14:57 -0400

    -Adds routes for Pushing from AMS2 to AAPB -Adds new search builder for 'needs_updating' search -Records last_updated date on Asset when Asset or its children are edited -Records last_pushed date when push is initiated for a record -Adds tests for Pushes

--- app/services/ams/export/export_service.rb
+++ app/services/ams/export/export_service.rb
@@ -62,7 +62,10 @@ module AMS
 
           output << `scp #{aapb_key_path} #{filepath} ec2-user@#{aapb_host}:/home/ec2-user/ingest_zips/#{filename}`
           output << `ssh -t #{aapb_key_path} ec2-user@#{aapb_host} 'cd /home/ec2-user/ingest_zips && unzip -o #{filename}'`
-          output << `ssh -t #{aapb_key_path} ec2-user@#{aapb_host} 'cd /var/www/aapb/current && RAILS_ENV=production ~/bin/bundle exec /usr/bin/ruby scripts/download_clean_ingest.rb --stdout-log --files /home/ec2-user/ingest_zips/*.xml' && cd /home/ec2-user/ingest_zips && rm -v ./*`
+          output << `ssh -t #{aapb_key_path} ec2-user@#{aapb_host} 'cd /var/www/aapb/current && RAILS_ENV=production ~/bin/bundle exec /usr/bin/ruby scripts/download_clean_ingest.rb --stdout-log --files /home/ec2-user/ingest_zips/*.xml'`
+
+          # TODO clean the dir?
+          #  && cd /home/ec2-user/ingest_zips && rm -v ./*
 
           # print and email
           Rails.logger.info output
