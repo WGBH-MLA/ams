691a50a9e3b7ec8234404902fb7bf2d9bbc52376
Andrew Myers <andrew_myers@wgbh.org>
2019-06-26 15:14:37 -0500

    Mods to VACUUM FULL process
    Did some refactoring of libs used for runnign VACUUM FULL on the Fedora DB,
    mainly to allow for more logging.

--- lib/ams/vacuum_full_fedora_db.rb
+++ lib/ams/vacuum_full_fedora_db.rb
@@ -21,29 +21,30 @@ module AMS
     def initialize(logger: nil)
       self.logger = logger if logger
       @vacuum_service = AMS::PostgresVacuumService.new(logger: self.logger)
-      @sidekiq_toggler = AMS::ToggleSidekiq.new(self.logger)
+      @sidekiq_toggler = AMS::ToggleSidekiq.new(logger: self.logger)
     end
 
     def run
-      unless vacuum_service.current_vacuum_full
-        logger.info "FULL VAC not running"
+      if vacuum_service.current_vacuum_full
+        logger.info "VACUUM FULL already running, doing nothing."
+        return
+      else
+        logger.info "Initiating VACUUM FULL for Fedora's Postgres DB..."
         sidekiq_toggler.turn_off if sidekiq_toggler.is_running?
-        logger.info "sidekiq turned off #{!sidekiq_toggler.is_running?}"
         vacuum_service.run_vacuum_full
-        logger.info "running that big bad vacuum"
         # sleep to give vacc time to start for while loop signal
         sleep 10
 
         while vacuum_service.current_vacuum_full
-          logger.info "sleeping..."
-          sleep 5
+          logger.info "Waiting for VACUUM FULL to complete..."
+          sleep 30
         end
         sidekiq_toggler.turn_on
-        logger.info "sidekiq turned on #{sidekiq_toggler.is_running?}"
+        logger.info "Finished VACUUM FULL of Fedora's Postgres DB."
       end
     end
 
-    # private
+    protected
 
       def logger=(logger)
         raise ArgumentError, "Logger object expected but #{logger.class} was given" unless logger.is_a? Logger
@@ -55,7 +56,3 @@ module AMS
       end
   end
 end
-
-# TODO: replace with relative path, not working now for some reason
-logger = Logger.new('/var/www/ams/log/vacuum.log', File::APPEND)
-AMS::VacuumFullFedoraDB.new(logger: logger).run
