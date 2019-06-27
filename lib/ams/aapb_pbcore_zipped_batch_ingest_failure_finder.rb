0cedfed3a222a3fc880cbb501f685454dcb4f270
Jason Corum <jason.corum@gmail.com>
2019-06-27 13:56:44 -0700

    uniq it

--- lib/ams/aapb_pbcore_zipped_batch_ingest_failure_finder.rb
+++ lib/ams/aapb_pbcore_zipped_batch_ingest_failure_finder.rb
@@ -2,7 +2,7 @@ module AMS
   class AAPBPBCoreZippedBatchIngestFailureFinder
 
     def self.find_failures
-      Hyrax::BatchIngest::BatchItem.where(:status => 'failed' ).map(&:id_within_batch) - Hyrax::BatchIngest::BatchItem.where(:status => 'completed' ).map(&:id_within_batch)
+      Hyrax::BatchIngest::BatchItem.where(:status => 'failed' ).map(&:id_within_batch).uniq - Hyrax::BatchIngest::BatchItem.where(:status => 'completed' ).map(&:id_within_batch).uniq
     end
 
   end
