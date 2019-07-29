4dbddc2aa03351c32a4d8687e9c7f6ebb6aa7464
Andrew Myers <andrew_myers@wgbh.org>
2019-07-29 15:22:50 -0400

    Fixes deployment
    * Add options to shebang of install.sh
      * -l ensures correct environment (namely RVM) is loaded.
      * -e ensures script exits when any command exits with non-zero. This makes for
        better logging, and fewer false positives.
    * Ensure bundler is installed before running `bundle install`.
    * Fixes verify_service.sh script run by CodeDeploy's ValidateService hook. It's
      now a minimal script that calls 2 rake tasks which check that Sidekiq is
      running and that the web app is reachable. No longer relies on Sidekiq pid
      file.
    * Fixes generation of deployment info page. Uses a rails generator now.

--- bin/deploy/install.sh
+++ bin/deploy/install.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
-/bin/bash --login
+#!/bin/bash -le
 sudo yum -y update
 source /etc/profile
 echo "Running Deployment for ID: $DEPLOYMENT_ID"
 export APP_HOME=/var/www/ams
 sudo chown -R ec2-user:ec2-user $APP_HOME
 cd $APP_HOME
+echo "Ensuring bundler is installed..."
+gem install bundler
 bundle install --deployment --without development test
 echo "ruby version:`ruby -v`"
 echo "rails versions:`bundle exec rails -v`"
@@ -17,8 +18,10 @@ if [ -z $SECRET_KEY_BASE ]; then
 sudo echo "export SECRET_KEY_BASE=`bundle exec rails secret`" >> /etc/profile
 source /etc/profile
 fi
-DEPLOYMENT_ID=$DEPLOYMENT_ID ruby bin/deploy/create_deployment_details_page.rb
+bin/rails g deployment_info_page --deployment_id $DEPLOYMENT_ID
 sudo service httpd restart
 sleep 2
 sudo service sidekiq restart
 sleep 2
+echo "End of install.sh"
+exit 0;
--- bin/deploy/verify_service.sh
+++ bin/deploy/verify_service.sh
@@ -1,34 +1,6 @@
-#!/bin/bash
-source /etc/profile
-sidekiq_pid_file=/var/www/ams/tmp/pids/sidekiq-ams.pid
-process_id=-1
-sidekiq_status=false
-
-result=$(curl -s -o /dev/null -I -w "%{http_code}" $PRODUCTION_HOST)
-
-if [ -f $sidekiq_pid_file ]; then
-    process_id=`cat $sidekiq_pid_file`
-fi
-
-if [[ result -eq 200 ]] && ([ -f $sidekiq_pid_file ] &&  [ -e /proc/$process_id ])
-then
-    echo "All Good."
-    exit 0
-else
-
-
-    if [[ result -ne 200 ]];then
-        output="Apache ERROR Logs\n\n$(tail -n 20 /var/log/httpd/error_log)\n\n\n"
-    fi
-
-    if [ ! -f $sidekiq_pid_file ] ||  [ ! -e /proc/$process_id ]
-    then
-        output+="Production Sidekiq Logs\n\n$(tail -n 20 /var/www/ams/log/sidekiq.log)\n\n\n"
-    fi
-
-
-    output+="Production Rails Logs\n\n$(tail -n 20 /var/www/ams/log/production.log)\n\n\n"
-
-    echo -e $output
-    exit 1
-fi
+#!/bin/bash -le
+cd /var/www/ams
+bin/rails verify_service:sidekiq
+bin/rails verify_service:homepage
+echo "End of verify_service.sh"
+exit 0;
