b2fe56cc2c4d36d957f803ae3579a906bcbdcbd6
Andrew Myers <andrew_myers@wgbh.org>
2019-09-29 11:51:38 -0400

    Several fixes to enable successful deployment
    Trying to get a deployment working, the install.sh script has been failing.
    Also, Travis has been failing intermittently on some feature specs.
    
    * Removes `pg` gem. We needed the 'pg' gem for vaccuuming the PostgreSQL
      database that Fedora was using. But we changed Fedora to use MySQL instead,
      precluding the need for vacuuming the DB, and the need for the `pg` gem.
    * Uses ENV.fetch rather than square brackets for access environment variables
      for email settings. When ENV vars are missing on host, the square brackets
      will return nil, which causes more falures if methods like .to_sym are called
      on those values. Using #fetch allows us to specify an empty string default.
    * Sets SECRET_KEY_BASE dynamically in install.sh, which is then used in
      config/secrets.yml to set the Rails `secret_key_base` options, which is then
      used by Devise's `secret_key` option. When SECRET_KEY_BASE env var is missing
      it triggers a Devise error.
    
    Also...
    * Fixes indentation in appspec.yml.
    * Removes unnecessary clicking of 'Asset Details' header in finnicky feature
      spec that sporadically fails on Travis.
    * Simplifies random string generator method used in specs.

--- bin/deploy/install.sh
+++ bin/deploy/install.sh
@@ -12,12 +12,14 @@ echo "ruby version:`ruby -v`"
 echo "rails versions:`bundle exec rails -v`"
 echo "node version:`node -v`"
 echo "yarn version:`yarn -v`"
+export SECRET_KEY_BASE=`bin/rails secret`
+# if [ -z $SECRET_KEY_BASE ]; then
+#   sudo echo "export SECRET_KEY_BASE=`bundle exec rails secret`" >> /etc/profile
+#   source /etc/profile
+# fi
 bundle exec rails db:migrate
 bundle exec rails assets:precompile
-if [ -z $SECRET_KEY_BASE ]; then
-sudo echo "export SECRET_KEY_BASE=`bundle exec rails secret`" >> /etc/profile
-source /etc/profile
-fi
+
 bin/rails g deployment_info_page --deployment_id $DEPLOYMENT_ID
 sudo service httpd restart
 sleep 2
