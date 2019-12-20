8b13abe1d3320da61464d6c52d741c9aaeee7400
foglabs <foglaboratories@gmail.com>
2019-12-20 13:26:38 -0500

    'pass in export type for documentstocsv'

--- app/services/ams/export/documents_to_csv.rb
+++ app/services/ams/export/documents_to_csv.rb
@@ -3,10 +3,10 @@ module AMS
     class DocumentsToCsv < ExportService
       attr_reader :object_type
 
-      def initialize(solr_documents, object_type:, filename: nil)
+      def initialize(solr_documents, object_type:, filename: nil, export_type: nil)
         raise ArgumentError.new("Not a valid object_type for CSV export") unless AMS::CsvExportExtension::CSV_FIELDS.has_key?(object_type)
         @object_type = object_type
-        super(solr_documents, format: format, filename: filename)
+        super(solr_documents, format: format, filename: filename, export_type: export_type)
       end
 
       def format
