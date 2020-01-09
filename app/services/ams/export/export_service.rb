d8f2d520fe55d0c995b1e4233f27ec3832d4b2d9
Henry Fog <foglaboratories@gmail.com>
2020-01-09 09:41:55 -0500

    Refactor export service (#417)
    * Removes block pattern from ExportService, replaces with explicit after-export options
    * Pass in export type for documentstocsv.
    * Don't pass file handle, just open up temp_file when its time to write.

--- app/services/ams/export/export_service.rb
+++ app/services/ams/export/export_service.rb
@@ -6,11 +6,19 @@ module AMS
       attr_reader :solr_documents
       attr_reader :format
       attr_reader :filename
-      attr_reader :file_path
       attr_reader :s3_path
+      attr_reader :export_type
 
-      def initialize(solr_documents, format:, filename: nil)
-        raise ArgumentError, ":format option required" unless format
+      attr_reader :temp_file_path
+
+      attr_accessor :export_data
+
+
+      # user is needed to 
+      def initialize(solr_documents, filename: nil, user: nil, export_type: nil)
+        # this is used for emailing push-to-aapb summaries in a PushedZip export_records_job
+        @user = user
+
         @solr_documents = solr_documents
         @format = format
         @filename = if filename.nil?
@@ -18,17 +26,41 @@ module AMS
                     else
                       filename
                     end
-        @file_path = Tempfile.new([@filename, ".#{@format}"])
+        @temp_file_path = Tempfile.new(@filename).path
         @s3_path = nil
+        @export_type = export_type
+        raise 'export_type was not defined!' unless @export_type
+
+        # call this my damn self
+        # this actually creates the export data, using a #process_export method defined in each subclass of ExportService
+        @export_data = process_export
+
+        if @export_type.end_with?('_job')
+          process_job
+        end
       end
 
-      def process
+      def format
+        raise 'Whoa there! Format is required! Did you define a #format method in your export adaptor class?'
+      end
+
+      def process_job
         begin
-          process_export
-          yield
+          # determine which after-package action to take
+
+          if @export_type == 'pushed_zip_job' # DocumentsToPushedZip 
+            
+            # send zip from tmp location to aapb
+            scp_to_aapb
+          elsif ['csv_job', 'pbcore_job'].include?(@export_type) # DocumentsToPbcoreXml or DocumentsToCsv
+
+            # upload zip to s3 for download
+            upload_to_s3
+            Ams2Mailer.export_notification(@user, @export_data.s3_path).deliver_later
+          end
         ensure
-          @file_path.close
-          @file_path.unlink # deletes the temp file.
+
+          File.delete(@temp_file_path)
         end
       end
 
@@ -39,10 +71,9 @@ module AMS
         )
         s3 = Aws::S3::Resource.new(region: 'us-east-1')
 
-        export_file = File.read(@file_path)
         # send file to s3
         obj = s3.bucket(ENV['S3_EXPORT_BUCKET']).object("#{ENV['S3_EXPORT_DIR']}/#{SecureRandom.uuid}/#{@filename}")
-        File.open(@file_path, 'r') do |f|
+        File.open(@temp_file_path, 'r') do |f|
           if format == 'csv'
             obj.upload_file(f, acl: 'public-read', content_disposition: 'attachment', content_type: 'text/csv')
           else
@@ -58,20 +89,19 @@ module AMS
         end
       end
 
-      def scp_to_aapb(user)
-        filepath = @file_path.path
+      def scp_to_aapb
 
-        if aapb_key_path && AMS::AAPB.reachable? && filepath.present?
+        if aapb_key_path && AMS::AAPB.reachable? && @temp_file_path.present?
           aapb_host = AMS::AAPB.host
           output = []
 
-          output << `scp #{aapb_key_path} #{filepath} ec2-user@#{aapb_host}:/home/ec2-user/ingest_zips/#{@filename}`
-          output << `ssh #{aapb_key_path} ec2-user@#{aapb_host} '/bin/bash cd /home/ec2-user/ingest_zips && unzip -o #{@filename}'`
-          output << `ssh #{aapb_key_path} ec2-user@#{aapb_host} '/bin/bash cd /var/www/aapb/current && RAILS_ENV=production ~/bin/bundle exec /usr/bin/ruby scripts/download_clean_ingest.rb --stdout-log --files /home/ec2-user/ingest_zips/*.xml'`
+          output << `scp #{aapb_key_path} #{@temp_file_path} ec2-user@#{aapb_host}:/home/ec2-user/ingest_zips/#{@filename}`
+          output << `ssh #{aapb_key_path} ec2-user@#{aapb_host} 'unzip -d /home/ec2-user/ingest_zips -o /home/ec2-user/ingest_zips/#{@filename}'`
+          output << `ssh #{aapb_key_path} ec2-user@#{aapb_host} 'cd /var/www/aapb/current && RAILS_ENV=production /home/ec2-user/.gem/ruby/gems/bundler-1.16.5/exe/bundle exec /usr/bin/ruby scripts/download_clean_ingest.rb --files /home/ec2-user/ingest_zips/*.xml'`
 
           # print and email
           Rails.logger.info output
-          Ams2Mailer.scp_to_aapb_notification(user, output.join("\n\n")).deliver_later
+          Ams2Mailer.scp_to_aapb_notification(@user, output.join("\n\n")).deliver_later
         else
           raise "AAPB was unreachable! #{ENV['AAPB_HOST']}"
         end
