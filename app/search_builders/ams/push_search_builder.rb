7e1fdaf52a8e468e07f0b7207070c5994e9a3498
foglabs <foglaboratories@gmail.com>
2020-01-22 16:12:10 -0500

    'delete comments'

--- app/search_builders/ams/push_search_builder.rb
+++ app/search_builders/ams/push_search_builder.rb
@@ -1,10 +1,7 @@
 module AMS
   class PushSearchBuilder < Hyrax::CatalogSearchBuilder
-    # this space intentionally left blank 
-        # Add date filters to the processor chain.
     self.default_processor_chain += [:pass_q_to_fq]
 
-    # Adds date filters to the :fq of the solr params.
     def pass_q_to_fq(solr_params)
       solr_params[:fq] = solr_params[:q]
       solr_params[:q] = ''
