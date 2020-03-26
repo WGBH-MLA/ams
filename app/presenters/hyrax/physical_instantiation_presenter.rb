0600ff9566d619c4d6ec2fabd3386af81235a150
Jason Corum <jason.corum@gmail.com>
2020-03-26 13:53:13 -0700

    Creates Renderer for attributes indexed to parent

--- app/presenters/hyrax/physical_instantiation_presenter.rb
+++ app/presenters/hyrax/physical_instantiation_presenter.rb
@@ -8,9 +8,40 @@ module Hyrax
              :language, :rights_summary, :rights_link, :annotation, :local_instantiation_identifier, :tracks, :channel_configuration,
              :alternative_modes, :holding_organization, :aapb_preservation_lto, :aapb_preservation_disk, to: :solr_document
 
+
     def attribute_to_html(field, options = {})
       options.merge!({:html_dl=> true})
-      super(field, options)
+
+      if attribute_indexed_to_parent?(field) && attribute_facetable?(field)
+        # Use :symbol for field_name since all attributes indexed to parent are indexed as symbols.
+        field_name = Solrizer.solr_name(field, :symbol)
+        # Use parent SolrDocument to get value
+        solr_document = ::SolrDocument.find(PhysicalInstantiation.find(id).member_of.first.id)
+
+        # Get values from sol_doc, should always be an Array since Assets can always have mutiple Instantiations
+        values = solr_document[field_name] || Array.new
+        return Hyrax::Renderers::IndexedToParentRenderer.new(field, values, options).render
+      else
+        return super(field, options)
+      end
+    end
+
+    def attributes_indexed_to_parent
+      PhysicalInstantiation.properties.select{ |k,v| v["index_to_parent"] == true unless v["index_to_parent"].nil? }
+    end
+
+    def attribute_indexed_to_parent?(field)
+      return true unless attributes_indexed_to_parent[field.to_s].nil?
+      false
+    end
+
+    def attribute_facetable?(field)
+      return true unless facetable_attributes[field.to_s].nil?
+      false
+    end
+
+    def facetable_attributes
+      PhysicalInstantiation.properties.select{ |k,v| v["behaviors"].include? :facetable unless v["behaviors"].nil? }
     end
   end
 end
