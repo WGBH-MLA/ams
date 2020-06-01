8eb02ce3a7fcf4600f92efb1a8295596938a8b9c
Jason Corum <jason.corum@gmail.com>
2020-06-01 15:24:01 -0700

    Moves duplicate method to module

--- app/presenters/aapb/instantiation_admin_data_presenter.rb
+++ app/presenters/aapb/instantiation_admin_data_presenter.rb
@@ -5,9 +5,32 @@ module AAPB
       def display_admin_data?
         !(aapb_preservation_lto.blank? &&
           aapb_preservation_disk.blank? &&
-          md5.blank?          
+          md5.blank?
         )
       end
+
+      def attribute_to_html(field, options = {})
+        require 'pry'; binding.pry
+        options.merge!({:html_dl=> true})
+
+        solr_document = ::SolrDocument.find(id)
+        work_class = solr_document["has_model_ssim"].first.constantize
+
+        if attribute_indexed_to_parent?(field, work_class) && attribute_facetable?(field, work_class)
+          # Use :symbol for field_name since all attributes indexed to parent are indexed as symbols.
+          field_name = Solrizer.solr_name(field, :symbol)
+
+          # Use parent SolrDocument to get value
+          solr_document = ::SolrDocument.find(work_class.find(id).member_of.first.id)
+
+          # Get values from sol_doc, should always be an Array since Assets can always have mutiple Instantiations
+          values = solr_document[field_name] || Array.new
+          return Hyrax::Renderers::IndexedToParentRenderer.new(field, values, options).render
+        else
+          return super(field, options)
+        end
+      end
+
     end
   end
 
--- app/presenters/hyrax/digital_instantiation_presenter.rb
+++ app/presenters/hyrax/digital_instantiation_presenter.rb
@@ -8,26 +8,5 @@ module Hyrax
     delegate :date, :dimensions, :digital_format, :standard, :location, :media_type, :generations, :file_size, :time_start, :duration,
              :data_rate, :colors, :language, :rights_summary, :rights_link, :annotation, :local_instantiation_identifier, :tracks,
              :channel_configuration, :alternative_modes, :holding_organization, :aapb_preservation_lto, :aapb_preservation_disk, :md5, to: :solr_document
-
-    def attribute_to_html(field, options = {})
-      options.merge!({:html_dl=> true})
-
-      solr_document = ::SolrDocument.find(id)
-      work_class = solr_document["has_model_ssim"].first.constantize
-
-      if attribute_indexed_to_parent?(field, work_class) && attribute_facetable?(field, work_class)
-        # Use :symbol for field_name since all attributes indexed to parent are indexed as symbols.
-        field_name = Solrizer.solr_name(field, :symbol)
-
-        # Use parent SolrDocument to get value
-        solr_document = ::SolrDocument.find(work_class.find(id).member_of.first.id)
-
-        # Get values from sol_doc, should always be an Array since Assets can always have mutiple Instantiations
-        values = solr_document[field_name] || Array.new
-        return Hyrax::Renderers::IndexedToParentRenderer.new(field, values, options).render
-      else
-        return super(field, options)
-      end
-    end
   end
 end
