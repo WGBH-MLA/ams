ef9c8618bdbbe67a694092ad9458a6a84303888f
Jason Corum <jason.corum@gmail.com>
2020-06-11 10:23:53 -0700

    434 Holding Org (#441)
    Adds organization to AAPBAdminData
    
    Also, Creates Renderer for attributes indexed to parent
    
    Closes #434

--- app/presenters/hyrax/digital_instantiation_presenter.rb
+++ app/presenters/hyrax/digital_instantiation_presenter.rb
@@ -3,14 +3,10 @@
 module Hyrax
   class DigitalInstantiationPresenter < Hyrax::WorkShowPresenter
     include AAPB::InstantiationAdminDataPresenter
+    include AAPB::AttributeIndexedToParentPresenter
 
     delegate :date, :dimensions, :digital_format, :standard, :location, :media_type, :generations, :file_size, :time_start, :duration,
              :data_rate, :colors, :language, :rights_summary, :rights_link, :annotation, :local_instantiation_identifier, :tracks,
              :channel_configuration, :alternative_modes, :holding_organization, :aapb_preservation_lto, :aapb_preservation_disk, :md5, to: :solr_document
-
-    def attribute_to_html(field, options = {})
-      options.merge!({:html_dl=> true})
-      super(field, options)
-    end
   end
 end
