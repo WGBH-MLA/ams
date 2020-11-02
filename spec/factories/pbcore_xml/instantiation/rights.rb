c9cc055884dc65504af9a32b4887e95d9c2ffd6f
foglabs <foglaboratories@gmail.com>
2020-11-02 15:29:34 -0500

    'redoing some lost fixes'

--- spec/factories/pbcore_xml/instantiation/rights.rb
+++ spec/factories/pbcore_xml/instantiation/rights.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
   factory :pbcore_instantiation_rights, class: PBCore::Instantiation::Rights, parent: :pbcore_element do
     skip_create
 
-    rights_summary { PBCore::RightsSummary::Summary.new(value: Faker::Movie.quote) }
-    rights_link { PBCore::RightsSummary::Link.new(value: Faker::Internet.url) }
+    summary { PBCore::RightsSummary::Summary.new(value: Faker::Movie.quote) }
+    link { PBCore::RightsSummary::Link.new(value: Faker::Internet.url) }
 
     initialize_with { new(attributes) }
   end
