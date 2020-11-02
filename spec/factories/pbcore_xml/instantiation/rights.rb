6a87607d68e3e297b57a468de2167bffd22a44e0
foglabs <foglaboratories@gmail.com>
2020-11-02 15:12:07 -0500

    'someone faker failure came back from revert...'

--- spec/factories/pbcore_xml/instantiation/rights.rb
+++ spec/factories/pbcore_xml/instantiation/rights.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :pbcore_instantiation_rights, class: PBCore::Instantiation::Rights, parent: :pbcore_element do
     skip_create
 
-    rights_summary { PBCore::RightsSummary::Summary.new(value: Faker::HitchhikersGuideToTheGalaxy.quote) }
+    rights_summary { PBCore::RightsSummary::Summary.new(value: Faker::Movie.quote) }
     rights_link { PBCore::RightsSummary::Link.new(value: Faker::Internet.url) }
 
     initialize_with { new(attributes) }
--- spec/factories/subject.rb
+++ spec/factories/subject.rb
@@ -4,7 +4,7 @@ require 'faker'
 FactoryBot.define do
   factory :pbcore_subject, class: PBCore::Subject, parent: :pbcore_element do
     skip_create
-    value { Faker::Movies::HitchhikersGuideToTheGalaxy.specie }
+    value { Faker::FunnyName.two_word_name }
     initialize_with { new(attributes) }
   end
 end
