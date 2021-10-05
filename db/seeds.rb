86ebaf9282830def409aa0bb2fde4c3147ad66c5
April Rieger <april@notch8.com>
2021-10-05 23:15:37 +0000

    fix-seeds-bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,52 @@
   { email: 'rob@notch8.com', password: 'testing123' },
   { email: 'support@notch8.com', password: 'testing123' },
   { email: 'admin@example.com', password: 'testing123' },
+  { email: 'wgbh_admin@wgbh-mla.org', password: 'pppppp' },
   { email: 'user@notch8.com', password: 'testing123' },
   { email: 'user@example.com', password: 'testing123' }
 ].each do |set|
   next if User.find_by(email: set[:email])
   user = User.create!(email: set[:email], password: set[:password])
 end
-admin = Role.find_or_create_by(name: 'admin')
-admin.users << User.find_by(email: 'rob@notch8.com')
-admin.users << User.find_by(email: 'support@notch8.com')
-admin.users << User.find_by(email: 'admin@example.com')
-admin.save
-aapb_admin = Role.find_or_create_by(name:'aapb-admin')
-aapb_admin.users << User.find_by(email: 'rob@notch8.com')
-aapb_admin.users << User.find_by(email: 'support@notch8.com')
-aapb_admin.users << User.find_by(email: 'admin@example.com')
-aapb_admin.save
+
+admin_role = Role.find_or_create_by(name: 'admin')
+aapb_admin_role = Role.find_or_create_by(name: 'aapb-admin')
+
+gbh_administrative_user = User.find_by(email:'wgbh_admin@wgbh-mla.org').roles
+notch8_admin = User.find_by(email: 'admin@example.com').roles
+notch8_support = User.find_by(email: 'support@notch8.com').roles
+rob_admin = User.find_by(email: 'rob@notch8.com').roles
+
+if gbh_administrative_user.any? {|r| r.name == 'admin'} == false
+  admin_role.users << User.find_by(email: 'wgbh_admin@wgbh-mla.org')
+end
+
+if gbh_administrative_user.any? {|r| r.name == 'aapb-admin'} == false
+  aapb_admin_role.users << User.find_by(email: 'wgbh_admin@wgbh-mla.org')
+end
+
+if notch8_admin.any? {|r| r.name == 'admin'} == false
+  admin_role.users << User.find_by(email: 'admin@example.com')
+end
+
+if notch8_admin.any? {|r| r.name == 'aapb-admin'} == false
+  aapb_admin_role.users << User.find_by(email: 'admin@example.com')
+end
+
+if notch8_support.any? {|r| r.name == 'admin'} == false
+  admin_role.users << User.find_by(email: 'support@notch8.com')
+end
+
+if notch8_support.any? {|r| r.name == 'aapb-admin'} == false
+  aapb_admin_role.users << User.find_by(email: 'support@notch8.com')
+end
+
+if rob_admin.any? {|r| r.name == 'admin'} == false
+  admin_role.users << User.find_by(email: 'rob@notch8.com')
+end
+
+if rob_admin.any? {|r| r.name == 'aapb-admin'} == false
+  aapb_admin_role.users << User.find_by(email: 'rob@notch8.com')
+end
+
+admin_set = AdminSet.find(AdminSet.find_or_create_default_admin_set_id)
