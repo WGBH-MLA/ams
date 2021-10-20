b229fba328093cc5b69be75b69c88ba7552f0ef7
Shana Moore <shana.lavina.moore@gmail.com>
2021-10-20 23:31:10 +0000

    I39 Refactor PBCore Parsers

--- app/parsers/pbcore_manifest_parser.rb
+++ app/parsers/pbcore_manifest_parser.rb
@@ -1,5 +1,7 @@
 class PbcoreManifestParser < Bulkrax::XmlParser
+  include Bulkrax::PbcoreParserBehavior
   attr_accessor :objects, :record_objects, :manifest_hash
+
   def create_works
     self.record_objects = []
     records.each_with_index do |file, index|
@@ -53,20 +55,6 @@ class PbcoreManifestParser < Bulkrax::XmlParser
       end
   end
 
-  # Return all files in the import directory and sub-directories
-  def file_paths
-    @file_paths ||=
-      # Relative to the file
-      if file? && zip?
-        Dir.glob("#{importer_unzip_path}/**/*").reject { |f| File.file?(f) == false }
-      elsif file? 
-        Dir.glob("#{File.dirname(parser_fields['import_file_path'])}/**/*").reject { |f| File.file?(f) == false }
-      # In the supplied directory
-      else
-        Dir.glob("#{parser_fields['import_file_path']}/**/*").reject { |f| File.file?(f) == false }
-      end
-  end
-
   # If the import_file_path is an xml file, return that
   # Otherwise return all xml files in the given folder
   def metadata_paths
@@ -79,42 +67,6 @@ class PbcoreManifestParser < Bulkrax::XmlParser
             f.include?("import_#{importerexporter.id}")
         end
       end
-  end
-
-  # Optional, only used by certain parsers
-  # Other parsers should override with a custom or empty method
-  # Will be skipped unless the #record is a Hash
-  def create_parent_child_relationships
-    parents.each do |key, value|
-      parent = entry_class.where(
-        identifier: key,
-        importerexporter_id: importerexporter.id,
-        importerexporter_type: 'Bulkrax::Importer'
-      ).first
-
-      # not finding the entries here indicates that the given identifiers are incorrect
-      # in that case we should log that
-      children = value.map do |child|
-        entry_class.where(
-          identifier: child,
-          importerexporter_id: importerexporter.id,
-          importerexporter_type: 'Bulkrax::Importer'
-        ).first
-      end.compact.uniq
-      
-      if parent.present? && (children.length != value.length)
-        # Increment the failures for the number we couldn't find
-        # Because all of our entries have been created by now, if we can't find them, the data is wrong
-        Rails.logger.error("Expected #{value.length} children for parent entry #{parent.id}, found #{children.length}")
-        break if children.empty?
-        Rails.logger.warn("Adding #{children.length} children to parent entry #{parent.id} (expected #{value.length})")
-      end
-      parent_id = parent.id
-      child_entry_ids = children.map(&:id)
-      Bulkrax::ChildRelationshipsJob.perform_later(parent_id, child_entry_ids, current_run.id)
-    end
-  rescue StandardError => e
-    status_info(e)
   end
 
   def setup_parents
@@ -178,43 +130,5 @@ class PbcoreManifestParser < Bulkrax::XmlParser
         md5: csv_row["DigitalInstantiation.md5"]
       ).gid
     end
-  end
-
-  def parse_rows(rows, type, index, current_object)
-    rows.each do |object|
-      set_model(type, index, object)
-      add_object(object.symbolize_keys)
-    end
-  end
-
-  def set_model(type, index, current_object)
-    if current_object && !current_object.keys.include?(:model)
-      key_count = objects.select { |obj| obj[:model] == type }.size + 1
-      current_object.merge!({
-        model: type,
-        work_identifier => Bulkrax.fill_in_blank_source_identifiers.call(self, "#{type}-#{index}-#{key_count}"),
-      })
-    end
-  end
-
-  def add_object(current_object)
-    if current_object.present?
-      if objects.first
-        objects.first[:children] ||= []
-        objects.first[:children] << current_object[work_identifier] if current_object[work_identifier].present?
-      end
-      record_objects << current_object
-      objects << current_object
-    end
-  end
-
-  def set_digital_instantiation_children(record)
-    child_identifer = record[work_identifier].gsub('DigitalInstantiation', 'EssenceTrack')
-    if objects.first[:children].include?(child_identifer)
-      record[:children] ||= []
-      record[:children] << child_identifer 
-      objects.first[:children].delete(child_identifer)
-    end
-    record_objects.find { |r| r[work_identifier] == record[work_identifier]}.merge!({ children: [child_identifer] })
   end
 end
