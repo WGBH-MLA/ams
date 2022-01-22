ed4dee3c6c0fb38d109266341fa75ca887c0c966
Alisha Evans <alisha@notch8.com>
2022-01-22 23:19:24 +0000

    64-xml-manifest

--- app/factories/bulkrax/object_factory.rb
+++ app/factories/bulkrax/object_factory.rb
@@ -15,16 +15,6 @@ Bulkrax::ObjectFactory.class_eval do     # rubocop:disable Metrics/ParameterList
       @klass = klass || Bulkrax.default_work_type.constantize
     end
 
-  def search_by_identifier
-    query = { "#{work_identifier}_sim" =>
-              source_identifier_value }
-    # Query can return partial matches (something6 matches both something6 and something68)
-    # so we need to weed out any that are not the correct full match. But other items might be
-    # in the multivalued field, so we have to go through them one at a time.
-    match = klass.where(query).detect { |m| m.send(work_identifier).include?(source_identifier_value) }
-    return match if match
-  end
-
   # Regardless of what the Parser gives us, these are the properties we are prepared to accept.
   def permitted_attributes
     klass.properties.keys.map(&:to_sym) + %i[id edit_users edit_groups read_groups visibility work_members_attributes admin_set_id member_of_collections_attributes pbcore_xml skip_file_upload_validation bulkrax_importer_id]
