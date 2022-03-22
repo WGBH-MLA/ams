f89e34894dbde3fb53e0881cd89cb45d30c69f77
Alisha Evans <alisha@notch8.com>
2022-03-22 18:54:28 -0500

    better comments and remove extra code

--- app/jobs/bulkrax/child_relationships_job_decorator.rb
+++ app/jobs/bulkrax/child_relationships_job_decorator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-# OVERRIDE bulkrax v.1.0.0
+# OVERRIDE bulkrax v.1.0.0 to add a limit to the job rescheduling
+# while forming relationships to child works that were found
 
 module Bulkrax
   module ChildRelationshipsJobDecorator
@@ -15,15 +16,14 @@ module Bulkrax
       # Not all of the Works/Collections exist yet; reschedule
     rescue Bulkrax::ChildWorksError
       # OVERRIDE bulkrax v.1.0.0
+      # In case the work hasn't been created, don't endlessly reschedule the job
       attempts = (args[3] || 0) + 1
       child_ids = @missing_entry_ids.presence || args[1]
 
-      # In case the work hasn't been created, don't endlessly reschedule the job
       reschedule(args[0], child_ids, args[2], attempts) unless attempts > 5
     end
 
     def work_membership
-      # add works to work
       members_works = []
       # reject any Collections, they can't be children of Works
       child_works_hash.each { |k, v| members_works << k if v[:class_name] != 'Collection' }
@@ -32,10 +32,12 @@ module Bulkrax
       end
       work_parent_work_child(members_works) if members_works.present?
       # OVERRIDE bulkrax v.1.0.0
+      # reschedule the job only with works that don't exist yet
       raise ChildWorksError if @missing_entry_ids.present?
     end
 
     # OVERRIDE bulkrax v.1.0.0
+    # don't stop all child relationships from being formed just because some child works don't exist
     def child_works_hash
       @missing_entry_ids = []
 
@@ -53,8 +55,6 @@ module Bulkrax
 
     private
 
-<<<<<<< HEAD
-=======
     def work_parent_work_child(member_ids)
       # build work_members_attributes
       binding.pry
@@ -75,7 +75,6 @@ module Bulkrax
       ImporterRun.find(importer_run_id).increment!(:failed_children)
     end
 
->>>>>>> d520c44 (stop the child relationships job from running endlessly)
     def reschedule(entry_id, child_entry_ids, importer_run_id, attempts)
       ChildRelationshipsJob.set(wait: 10.minutes).perform_later(entry_id, child_entry_ids, importer_run_id, attempts)
     end
