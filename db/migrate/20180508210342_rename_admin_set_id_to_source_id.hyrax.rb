4705864c147beeeadc525a984d65b145329fd1de
Rob Kaufman <rob@notch8.com>
2022-11-11 13:06:30 -0800

    fix permission template reload issue

--- db/migrate/20180508210342_rename_admin_set_id_to_source_id.hyrax.rb
+++ db/migrate/20180508210342_rename_admin_set_id_to_source_id.hyrax.rb
@@ -1,5 +1,6 @@
 class RenameAdminSetIdToSourceId < ActiveRecord::Migration[5.1]
   def change
     rename_column :permission_templates, :admin_set_id, :source_id
+    Hyrax::PermissionTemplate.reset_column_information
   end
 end
