c6e0bbd663c3b41d115e9ab31dc2f3d4a78ddece
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2023-08-09 18:13:46 -0700

    extract :intended_children_count from original Bulkrax::Entry source data (XML only)

--- app/services/ams/backfill_asset_validation_status.rb
+++ app/services/ams/backfill_asset_validation_status.rb
@@ -53,33 +53,47 @@ module AMS
 
     def backfill_validation_status(id)
       asset = Asset.find(id)
-      attrs = {}
+      attrs_for_actor = {}
+      raise StandardError, "Unable to find admin data for Asset #{asset.id}" if asset.admin_data.blank?
 
-      source = if asset.admin_data.bulkrax_importer_id.present?
-                 importer = Bulkrax::Importer.find(asset.admin_data.bulkrax_importer_id)
-                 importer.entries.where("JSON_EXTRACT(raw_metadata, '$.local_identifier') = '#{asset.local_identifier.first}'").last
-               else
-                 Hyrax::BatchIngest::BatchItem.find_by(repo_object_id: asset.id)
-               end
+      raw_data = if asset.admin_data.bulkrax_importer_id.present?
+                   # TODO: extract
+                   importer = Bulkrax::Importer.find(asset.admin_data.bulkrax_importer_id)
+                   resp = importer.entries.select(:id).where("JSON_EXTRACT(parsed_metadata, '$.id') = '#{asset.id}'")
+                   raise StandardError, "Ambiguous data sources found for Asset #{asset.id}" if resp.count > 1
 
-      if source.is_a?(Bulkrax::Entry)
-        # TODO: extract :intended_children_count from source data
-      elsif source.is_a?(Hyrax::BatchIngest::BatchItem)
-        data = File.read(source.source_location)
-        asset_xml_attrs = AAPB::BatchIngest::PBCoreXMLMapper.new(data).asset_attributes
-        return if asset_xml_attrs[:intended_children_count].blank?
+                   source = Bulkrax::Entry.find(resp.first.try(:id))
+                   ## NOTE:
+                   # As of 9 August, 2023, all Bulkrax entries in production are instances of Bulkrax::PbcoreXmlEntry, thus
+                   # we can safely assume that the data we're after won't be anywhere other than in `raw_metadata['pbcore_xml']`
+                   source.raw_metadata['pbcore_xml']
+                 elsif asset.admin_data.hyrax_batch_ingest_batch_id.present?
+                   # TODO: extract
+                   batch = Hyrax::BatchIngest::Batch.find(asset.admin_data.hyrax_batch_ingest_batch_id)
+                   ## NOTE:
+                   # As of 9 August, 2023, the logic to count the number of intended children an Asset should have has only
+                   # been applied to PBCore XML BatchIngests. Once this logic has been applied to other types of ingests
+                   # (CSV, etc.), this short-circuit should be removed and this class should be modified to extrac the data
+                   # from more than just PBCore XML.
+                   # @see https://github.com/scientist-softserv/ams/issues/9
+                   if batch.ingest_type != 'aapb_pbcore_zipped'
+                     raise StandardError, "Don't know how to count intended children when BatchIngest type is #{batch.ingest_type}"
+                   end
 
-        attrs['intended_children_count'] = asset_xml_attrs[:intended_children_count]
-      else
-        # TODO: figure out how to set :intended_children_count when source data can't be found (possible?)
-        raise StandardError, "Unable to find source data for Asset #{asset.id}" if source.blank?
-      end
+                   source = batch.batch_items.find_by(repo_object_id: asset.id)
+                   File.read(source.source_location)
+                 else
+                   raise StandardError, "Unable to find source data for Asset #{asset.id}"
+                 end
 
-      raise StandardError, "Unable to count intended children for Asset #{asset.id}" if attrs['intended_children_count'].blank?
+      asset_xml_attrs = AAPB::BatchIngest::PBCoreXMLMapper.new(raw_data).asset_attributes
+      attrs_for_actor['intended_children_count'] = asset_xml_attrs[:intended_children_count]
+      raise StandardError, "Unable to count intended children for Asset #{asset.id}" if attrs_for_actor['intended_children_count'].blank?
 
+      # Generic admin user we can count on existing
       user = User.find_by(email: 'wgbh_admin@wgbh-mla.org')
       actor = Hyrax::CurationConcern.actor
-      env = Hyrax::Actors::Environment.new(asset, Ability.new(user), attrs)
+      env = Hyrax::Actors::Environment.new(asset, Ability.new(user), attrs_for_actor)
 
       actor.update(env)
     end
