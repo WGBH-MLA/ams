4e838588734fac8ef13a9e6658e96719734967f7
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2023-08-10 17:05:22 -0700

    failedBackfillAssetValidationStatusJobs log errors

--- app/jobs/backfill_asset_validation_status_job.rb
+++ app/jobs/backfill_asset_validation_status_job.rb
@@ -11,5 +11,15 @@ class BackfillAssetValidationStatusJob < ApplicationJob
     env = Hyrax::Actors::Environment.new(asset, Ability.new(user), attrs_for_actor)
 
     actor.update(env)
+  rescue => e
+    logger.error("Asset update failed! | #{e.class} | #{e.message} | #{asset_id}")
+    raise e
+  end
+
+  # This logger is shared with AMS::BackfillAssetValidationStatus
+  def logger
+    @logger ||= ActiveSupport::Logger.new(
+      AMS::BackfillAssetValidationStatus::LOGGER_PATH
+    )
   end
 end
