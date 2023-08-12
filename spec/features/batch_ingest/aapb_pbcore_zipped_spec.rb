5ac83eef15e4c5a84c7137b83b0b1b810094b197
Kirk Wang <kirk.wang@scientist.com>
2023-08-12 07:00:25 -0700

    ✅ Use `Sipity.Agent` instead of `PowerConverter`
    This commit updates the `#sipity_agent` method to use `Sipity.Agent` in
    Hyrax 4.0 because `PowerConverter` is deprecated.  This also fixed the
    previously skipped `aapb_pbcore_zipped_spec`.
    
    Ref:
      - https://github.com/samvera/hyrax/pull/5980

--- spec/features/batch_ingest/aapb_pbcore_zipped_spec.rb
+++ spec/features/batch_ingest/aapb_pbcore_zipped_spec.rb
@@ -5,96 +5,95 @@ require 'aapb/batch_ingest'
 RSpec.feature "Ingest: AAPB PBCore - Zipped" do
 
   context 'where batch contains valid Assets (no children)', reset_data: false do
-    if App.rails_5_1?
-      before :all do
-        # Build a list of PBCore Description Documents, keeping it pretty small to
-        # avoid long ingest times.
-        @pbcore_description_documents = build_list(:pbcore_description_document, rand(2..4), :full_aapb)
-        @pbcore_description_documents.each do |pbcore_description_document|
-          pbcore_description_document.instantiations = [
-            # Add 1-3 Digital Instantiations
-            build_list(:pbcore_instantiation, rand(1..3), :digital),
-            # Add 1 Physical Instantiation
-            build(:pbcore_instantiation, :physical)
-          ].flatten
 
-          # Add 1-3 Essence Tracks for each instantiation
-          pbcore_description_document.instantiations.each do |instantiation|
-            instantiation.essence_tracks = build_list(:pbcore_instantiation_essence_track, rand(1..3))
-          end
-        end
+    before :all do
+      # Build a list of PBCore Description Documents, keeping it pretty small to
+      # avoid long ingest times.
+      @pbcore_description_documents = build_list(:pbcore_description_document, rand(2..4), :full_aapb)
+      @pbcore_description_documents.each do |pbcore_description_document|
+        pbcore_description_document.instantiations = [
+          # Add 1-3 Digital Instantiations
+          build_list(:pbcore_instantiation, rand(1..3), :digital),
+          # Add 1 Physical Instantiation
+          build(:pbcore_instantiation, :physical)
+        ].flatten
 
-        zipped_batch = make_aapb_pbcore_zipped_batch(@pbcore_description_documents)
-
-        user, admin_set = create_user_and_admin_set_for_deposit
-        @batch = run_batch_ingest(ingest_file_path: zipped_batch,
-                                  ingest_type: 'aapb_pbcore_zipped',
-                                  admin_set: admin_set,
-                                  submitter: user)
-
-        # Reload the batch so all the child BatchItems are populated.
-        @batch.reload
-
-        # Grab the ingested objects from the BatchItem's #repo_object_id values.
-        # We store in an instance var to use it more than once between tests.
-        # We rescue from any ObjectNotFound error to mystery errors in the before
-        # hook; we would rather have failed tests.
-        @ingested_objects = @batch.batch_items.map do |batch_item|
-          ActiveFedora::Base.find(batch_item.repo_object_id.to_s)
-        rescue ActiveFedora::ObjectNotFoundError
-          nil
-        end.compact
-
-      end
-
-      let(:expected_batch_item_count) do
-        instantiations = @pbcore_description_documents.map(&:instantiations).flatten
-        physical_instantiations = instantiations.select { |i| i.physical }
-        physical_instantiation_essence_tracks = physical_instantiations.map(&:essence_tracks).flatten
-        # The expected number of batch items is the  number of Assets, the number
-        # of Instantiations, and the number of  Essence Tracks from Physical
-        # Instantiations only. A little odd, but this is just how the ingest works
-        # right now.
-        @pbcore_description_documents.count \
-          + instantiations.count \
-          + physical_instantiation_essence_tracks.count
-      end
-
-      it 'creates the correct number of batch item records' do
-        expect(@batch.batch_items.to_a.count).to eq expected_batch_item_count
-      end
-
-      it 'ingests the correct number of objects' do
-        expect(@ingested_objects.count).to eq expected_batch_item_count
-      end
-
-      it 'creates additional BatchItem records that all share the same `id_within_batch` value' do
-        batch_items_by_repo_object_id = @batch.batch_items.index_by(&:repo_object_id)
-        ingested_objects_by_id = @ingested_objects.index_by(&:id)
-        batch_items_by_repo_object_id.each do |repo_object_id, batch_item|
-          # There should be at most 1 parent work ID; assets won't have one.
-          parent_work_id = ingested_objects_by_id[repo_object_id].parent_work_ids.first
-          if parent_work_id
-            parent_batch_item = batch_items_by_repo_object_id[parent_work_id]
-            expect(batch_item.id_within_batch).to eq parent_batch_item.id_within_batch
-          end
+        # Add 1-3 Essence Tracks for each instantiation
+        pbcore_description_document.instantiations.each do |instantiation|
+          instantiation.essence_tracks = build_list(:pbcore_instantiation_essence_track, rand(1..3))
         end
       end
 
-      it 'has a status of completed' do
-        expect(@batch.status).to eq "completed"
-      end
+      zipped_batch = make_aapb_pbcore_zipped_batch(@pbcore_description_documents)
 
-      it 'has no errors for any batch item' do
-        expect(@batch.batch_items.map(&:error)).to all(be_nil)
-      end
+      user, admin_set = create_user_and_admin_set_for_deposit
+      @batch = run_batch_ingest(ingest_file_path: zipped_batch,
+                                ingest_type: 'aapb_pbcore_zipped',
+                                admin_set: admin_set,
+                                submitter: user)
 
-      it 'has status of "completed" for each batch item' do
-        expect(@batch.batch_items.map(&:status)).to all( eq 'completed' )
+      # Reload the batch so all the child BatchItems are populated.
+      @batch.reload
+
+      # Grab the ingested objects from the BatchItem's #repo_object_id values.
+      # We store in an instance var to use it more than once between tests.
+      # We rescue from any ObjectNotFound error to mystery errors in the before
+      # hook; we would rather have failed tests.
+      @ingested_objects = @batch.batch_items.map do |batch_item|
+        ActiveFedora::Base.find(batch_item.repo_object_id.to_s)
+      rescue ActiveFedora::ObjectNotFoundError
+        nil
+      end.compact
+
+    end
+
+    let(:expected_batch_item_count) do
+      instantiations = @pbcore_description_documents.map(&:instantiations).flatten
+      physical_instantiations = instantiations.select { |i| i.physical }
+      physical_instantiation_essence_tracks = physical_instantiations.map(&:essence_tracks).flatten
+      # The expected number of batch items is the  number of Assets, the number
+      # of Instantiations, and the number of  Essence Tracks from Physical
+      # Instantiations only. A little odd, but this is just how the ingest works
+      # right now.
+      @pbcore_description_documents.count \
+        + instantiations.count \
+        + physical_instantiation_essence_tracks.count
+    end
+
+    it 'creates the correct number of batch item records' do
+      expect(@batch.batch_items.to_a.count).to eq expected_batch_item_count
+    end
+
+    it 'ingests the correct number of objects' do
+      expect(@ingested_objects.count).to eq expected_batch_item_count
+    end
+
+    it 'creates additional BatchItem records that all share the same `id_within_batch` value' do
+      batch_items_by_repo_object_id = @batch.batch_items.index_by(&:repo_object_id)
+      ingested_objects_by_id = @ingested_objects.index_by(&:id)
+      batch_items_by_repo_object_id.each do |repo_object_id, batch_item|
+        # There should be at most 1 parent work ID; assets won't have one.
+        parent_work_id = ingested_objects_by_id[repo_object_id].parent_work_ids.first
+        if parent_work_id
+          parent_batch_item = batch_items_by_repo_object_id[parent_work_id]
+          expect(batch_item.id_within_batch).to eq parent_batch_item.id_within_batch
+        end
       end
-    # TODO: Tests for invalid Assets.
-    else
-      skip 'Until we decide if we keep Hyrax::BatchIngest, see: https://github.com/scientist-softserv/ams/issues/58'
+    end
+
+    it 'has a status of completed' do
+      expect(@batch.status).to eq "completed"
+    end
+
+    it 'has no errors for any batch item' do
+      expect(@batch.batch_items.map(&:error)).to all(be_nil)
+    end
+
+    it 'has status of "completed" for each batch item' do
+      expect(@batch.batch_items.map(&:status)).to all( eq 'completed' )
     end
   end
+
+  # TODO: Tests for invalid Assets.
+
 end
