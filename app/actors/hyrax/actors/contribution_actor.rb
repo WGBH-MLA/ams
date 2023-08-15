0e322548b498ece893afda5ac6a0cf978466e2a4
LaRita Robinson <larita@scientist.com>
2023-08-15 16:27:29 -0400

    Remove actor customizations
    Refs https://github.com/scientist-softserv/ams/issues/30
    Actor changes dealing with nested indexing are obsolete with upgrade.

--- app/actors/hyrax/actors/contribution_actor.rb
+++ app/actors/hyrax/actors/contribution_actor.rb
@@ -4,13 +4,18 @@ module Hyrax
   module Actors
     class ContributionActor < Hyrax::Actors::BaseActor
       def create(env)
-        # queue indexing if we are importing
-        env.curation_concern.reindex_extent = "queue#{env.importing.id}" if env.importing
+        if App.rails_5_1?
+          # queue indexing if we are importing
+          env.curation_concern.reindex_extent = "queue#{env.importing.id}" if env.importing
+        end
         super
       end
 
       def update(env)
-        env.curation_concern.reindex_extent = "queue#{env.importing.id}" if env.importing
+        if App.rails_5_1?
+          # queue indexing if we are importing
+          env.curation_concern.reindex_extent = "queue#{env.importing.id}" if env.importing
+        end
         super
       end
     end
