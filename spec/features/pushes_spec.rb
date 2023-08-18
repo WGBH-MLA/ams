f1c7dc244a8b8167a396e38af5d1b6c2c854c214
Kirk Wang <kirk.wang@scientist.com>
2023-08-17 20:00:22 -0700

    ✅ Fix `pushes_spec`
    This commit fixes the `pushes_spec` by adjusting the specifics a little
    bit. In Hyrax 4/Blacklight 7 the catalog search input has a different
    `id` but the `name` remains the same as in previous versions so we'll
    use that instead.  I had changed the `Push To AAPB` link to a button
    previous so this commit will address that.  Now instead of using
    `#click_link` or `#click_button` we'll use `#find` and `#click` to
    activate it so it works for both versions of the app.

--- spec/features/pushes_spec.rb
+++ spec/features/pushes_spec.rb
@@ -4,71 +4,65 @@ RSpec.describe "Pushes features", type: :controller, js: true do
   include Warden::Test::Helpers
   include Devise::Test::ControllerHelpers
 
-  if App.rails_5_1?
-    # let it bang
-    let!(:user) { create :admin_user }
-    let!(:asset) { create(:asset, user: user, program_title: ['foo'], validation_status_for_aapb: [Asset::VALIDATION_STATUSES[:valid]]) }
-    let!(:asset2) { create(:asset, user: user, program_title: ['foo bar'], validation_status_for_aapb: [Asset::VALIDATION_STATUSES[:valid]]) }
-    let!(:asset3) { create(:asset, user: user, needs_update: true, validation_status_for_aapb: [Asset::VALIDATION_STATUSES[:valid]]) }
+  # let it bang
+  let!(:user) { create :admin_user }
+  let!(:asset) { create(:asset, user: user, program_title: ['foo'], validation_status_for_aapb: [Asset::VALIDATION_STATUSES[:valid]]) }
+  let!(:asset2) { create(:asset, user: user, program_title: ['foo bar'], validation_status_for_aapb: [Asset::VALIDATION_STATUSES[:valid]]) }
+  let!(:asset3) { create(:asset, user: user, needs_update: true, validation_status_for_aapb: [Asset::VALIDATION_STATUSES[:valid]]) }
 
-    # Login/logout before/after each test.
-    before { login_as(user) }
-    after { Warden.test_reset! }
+  # Login/logout before/after each test.
+  before { login_as(user) }
+  after { Warden.test_reset! }
 
-    context '#pushes' do
+  context '#pushes' do
 
-      it 'gives validation error when invalid GUID input data' do
-        visit '/pushes/new'
-        bad_ids = [ 'blerg', 'cpb-aacip-11111111111' ]
-        fill_in(id: 'id_field', with: bad_ids.join("\n") )
-        expect(page).to have_text "The following IDs are not found"
-        bad_ids.each do |bad_id|
-          expect(page).to have_text bad_id
-        end
-      end
-
-      it 'gives all clear for valid GUID input data' do
-        visit '/pushes/new'
-        fill_in(id: 'id_field', with: asset.id )
-        expect(page).to have_text('All GUIDs are valid!')
-      end
-
-      it 'gets the correct record set for needs_updating' do
-        # this i do
-        # just for u
-        visit '/pushes/needs_updating'
-        expect(page.find('textarea')).to have_text(asset3.id)
-      end
-
-      it 'gets the correct record set when navigating from a catalog search' do
-        # uri = %(/catalog?q=foo)
-        # visit uri
-        visit '/catalog'
-        fill_in(id: 'q', with: 'foo')
-        click_button(id: 'search-submit-header')
-        click_link('Push To AAPB', class: 'aapb-push-button')
-
-        expect(page.find('textarea')).to have_text(asset.id)
-        expect(page.find('textarea')).to have_text(asset2.id)
-      end
-
-      it 'can submit a push successfully' do
-        allow(PushToAAPBJob).to receive(:perform_later)
-        visit '/pushes/new'
-        fill_in('id_field', with: asset.id )
-        click_button(id: 'push-submit')
-
-        # this will have the output mail
-        # output_mail = ActionMailer::Base.deliveries.last
-        expect(PushToAAPBJob).to have_received(:perform_later)
-        push = Push.last
-        expect(push.user_id).to eq(user.id)
-        expect(push.pushed_id_csv).to eq(asset.id)
+    it 'gives validation error when invalid GUID input data' do
+      visit '/pushes/new'
+      bad_ids = [ 'blerg', 'cpb-aacip-11111111111' ]
+      fill_in(id: 'id_field', with: bad_ids.join("\n") )
+      expect(page).to have_text "The following IDs are not found"
+      bad_ids.each do |bad_id|
+        expect(page).to have_text bad_id
       end
     end
-  else
-    skip 'Skipping tests until bootstrap upgrade is complete'
-    # ref: https://github.com/scientist-softserv/ams/issues/28
-    # ref: https://github.com/scientist-softserv/ams/issues/32
+
+    it 'gives all clear for valid GUID input data' do
+      visit '/pushes/new'
+      fill_in(id: 'id_field', with: asset.id )
+      expect(page).to have_text('All GUIDs are valid!')
+    end
+
+    it 'gets the correct record set for needs_updating' do
+      # this i do
+      # just for u
+      visit '/pushes/needs_updating'
+      expect(page.find('textarea')).to have_text(asset3.id)
+    end
+
+    it 'gets the correct record set when navigating from a catalog search' do
+      # uri = %(/catalog?q=foo)
+      # visit uri
+      visit '/catalog'
+      fill_in(name: 'q', with: 'foo')
+      click_button(id: 'search-submit-header')
+      find('.aapb-push-button').click
+
+      expect(page.find('textarea')).to have_text(asset.id)
+      expect(page.find('textarea')).to have_text(asset2.id)
+    end
+
+    it 'can submit a push successfully' do
+      allow(PushToAAPBJob).to receive(:perform_later)
+      visit '/pushes/new'
+      fill_in('id_field', with: asset.id )
+      click_button(id: 'push-submit')
+
+      # this will have the output mail
+      # output_mail = ActionMailer::Base.deliveries.last
+      expect(PushToAAPBJob).to have_received(:perform_later)
+      push = Push.last
+      expect(push.user_id).to eq(user.id)
+      expect(push.pushed_id_csv).to eq(asset.id)
+    end
   end
 end
