7fb777dd5352699b3398db862eb8da37fb6807e0
Shana Moore <shana@scientist.com>
2023-08-23 12:19:17 -0700

    :gear: add sentry config file
    Locally I was successful at capturing an error in sentry after including this file.

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,15 @@
+# in Rails, this might be in config/initializers/sentry.rb
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.environment = ENV['SENTRY_ENVIRONMENT']
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
