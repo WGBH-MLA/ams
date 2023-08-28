19950e3b27cdf85764e5c3b51e4c1a94e1b3abe7
Kirk Wang <kirk.wang@scientist.com>
2023-08-28 15:43:50 -0700

    🐛 Fix `hyrax_batch_ingest_batch_id` facet
    This commit will add the `hyrax_batch_ingest_batch_id` facet to the
    `CatalogController` so that it can be used in the UI.  It was not
    working when it was simply added in the `Hyrax::MyController`, so it
    maybe not be relevant anymore and can be removed from there.
    
    Ref:
      - https://github.com/scientist-softserv/ams/issues/100

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -75,6 +75,7 @@ class CatalogController < ApplicationController
     config.add_facet_field "language_ssim", label: "Language", limit: 5, collapse: true
     config.add_facet_field "level_of_user_access_ssim", label: "Level of user access", limit: 5, collapse: true
     config.add_facet_field "transcript_status_ssim", label: "Transcript Status", limit: 5, collapse: true
+    config.add_facet_field "hyrax_batch_ingest_batch_id_tesim"
 
     config.add_facet_field 'cataloging_status_ssim', query: {
         yes: { label: 'Yes', fq: 'cataloging_status_ssim:Yes' },
