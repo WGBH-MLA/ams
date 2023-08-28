3c059376de6cbcd686955ab257e9de4f4ffd387f
Shana Moore <shana@scientist.com>
2023-08-28 16:52:10 -0700

    :broom: update method signature to be more in line with Bulkrax 5.3.0
    I tested this with Bulkrax 1.0.2 and Bulkrax 5.3.0.

--- app/factories/bulkrax/object_factory.rb
+++ app/factories/bulkrax/object_factory.rb
@@ -4,7 +4,7 @@ require_dependency Bulkrax::Engine.root.join('app', 'factories', 'bulkrax', 'obj
 
 Bulkrax::ObjectFactory.class_eval do     # rubocop:disable Metrics/ParameterLists
     # rubocop:disable Metrics/ParameterLists
-    def initialize(attributes:, source_identifier_value:, work_identifier:, related_parents_parsed_mapping: nil, collection_field_mapping:, replace_files: false, user: nil, klass: nil, update_files: false, importer: nil)
+    def initialize(attributes:, source_identifier_value:, work_identifier:, related_parents_parsed_mapping: nil, collection_field_mapping:, replace_files: false, user: nil, klass: nil, importer_run_id: nil, update_files: false, importer: nil)
       @attributes = ActiveSupport::HashWithIndifferentAccess.new(attributes)
       @replace_files = replace_files
       @update_files = update_files
