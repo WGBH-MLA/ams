085ecf84490e2c95a294b76ca13d884773ef1cd6
Kirk Wang <kirk.wang@scientist.com>
2023-09-05 23:04:04 -0700

    Get all work types to import

--- app/models/asset_resource.rb
+++ app/models/asset_resource.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
-  
+
 # Generated via
 #  `rails generate hyrax:work_resource AssetResource`
 class AssetResource < Hyrax::Work
   include Hyrax::Schema(:basic_metadata)
   include Hyrax::Schema(:asset_resource)
+
+  def admin_data
+    @admin_data ||= AdminData.find_by_gid(admin_data_gid)
+  end
+
+  def admin_data=(new_admin_data)
+    self.admin_data_gid = new_admin_data.gid
+  end
 end
