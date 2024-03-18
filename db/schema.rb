6d8253aad783da99093a92a84e5322a81522688c
Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2024-03-18 10:21:57 -0700

    updated db schema (#845)
    Reflects migration added by:
    - https://github.com/WGBH-MLA/ams/pull/841

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_30_155065) do
+ActiveRecord::Schema.define(version: 2024_03_07_194231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -414,6 +414,7 @@ ActiveRecord::Schema.define(version: 2023_08_30_155065) do
     t.datetime "updated_at", null: false
     t.string "internal_resource"
     t.integer "lock_version"
+    t.index "(((metadata -> 'bulkrax_identifier'::text) ->> 0))", name: "index_on_bulkrax_identifier", where: "((metadata -> 'bulkrax_identifier'::text) IS NOT NULL)"
     t.index ["internal_resource"], name: "index_orm_resources_on_internal_resource"
     t.index ["metadata"], name: "index_orm_resources_on_metadata", using: :gin
     t.index ["metadata"], name: "index_orm_resources_on_metadata_jsonb_path_ops", opclass: :jsonb_path_ops, using: :gin
