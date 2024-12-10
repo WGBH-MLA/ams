e16bdafc2ace5e63824958787f04b45188ca1ebc
Andrew Myers <afredmyers@gmail.com>
2024-12-10 15:50:03 -0500

    Gets specs passing for PBCoreXMLInstantiationReset ingester

--- spec/services/aapb/batch_ingest/pbcore_xml_instantiation_reset_spec.rb
+++ spec/services/aapb/batch_ingest/pbcore_xml_instantiation_reset_spec.rb
@@ -35,10 +35,7 @@ RSpec.describe AAPB::BatchIngest::PBCoreXMLInstantiationReset, reset_data: false
             build(
               :pbcore_instantiation,
               :digital,
-              # Because we are ingesting we should not have an AAPB ID identifier,
-              # which means we have to explicitly specify our identifiers because an
-              # AAPB ID in in factory-generated instantiations added by default.
-              identifiers: [ build(:pbcore_instantiation_identifier) ]
+              identifiers: [ build(:pbcore_instantiation_identifier, source: 'test-local', value: SecureRandom.hex(5)) ]
             )
           end,
 
@@ -49,8 +46,8 @@ RSpec.describe AAPB::BatchIngest::PBCoreXMLInstantiationReset, reset_data: false
               :physical,
               # Because we are ingesting we should not have an AAPB ID identifier,
               # which means we have to explicitly specify our identifiers because an
-              # AAPB ID in in factory-generated instantiations added by default.
-              identifiers: [ build(:pbcore_instantiation_identifier) ]
+              # AAPB ID is included in factory-generated instantiations by default.
+              identifiers: [ build(:pbcore_instantiation_identifier, source: 'test-local', value: SecureRandom.hex(5)) ]
             )
           end
           # Flatten the 2 lists of instantiations into a single list.
@@ -74,33 +71,20 @@ RSpec.describe AAPB::BatchIngest::PBCoreXMLInstantiationReset, reset_data: false
     end
   end
 
-  let(:orig_instantiations) do
+  let(:expected_inst_local_ids) do
     pbcore_docs
-      .first
-      .instantiations
-      .select { |i| i.digital }
+      .last                               # The last one used for PBCoreXMLInstantiationResetIngester (see above)
+      .instantiations                     # map the docs to instantiations
+      .map { |i| i.identifiers }.flatten  # map the instantiations to their identifiers
+      .select { |i| i.source != 'ams' }   # select only the non-AMS identifiers (i.e. local identifiers)
+      .map { |i| i.value }                # map to the unique values for comparison in tests.
   end
 
-  # expected_instantiations - PBCore XML of the new digital
-  # instantiations being input for ingestion (i.e. the expected result when
-  # comparing for correctness)
-  let(:expected_instantiations) do
-    pbcore_docs
-      .last
-      .instantiations
-      .select { |i| i.digital }
-  end
+  let(:actual_inst_local_ids) do
+    ar = AssetResource.find(pbcore_identifier.value)
+    instantiations = ar.digital_instantiation_resources + ar.physical_instantiation_resources
 
-  # actual_instantiations - PBCore XML of the new instantiations
-  # after ingestion (i.e. the actual result when comparing for correctness)
-  # NOTE: depends on `before` block below running successfully, which it should
-  # do automatically prior to each example within the same context, including
-  # nested contexts.
-  let(:actual_instantiations) do
-    PBCore::DescriptionDocument.parse(
-      SolrDocument.find(pbcore_identifier.value).export_as_pbcore
-    ).instantiations
-      .select { |i| i.digital }
+    instantiations.map { |i| i.local_instantiation_identifier }.flatten
   end
 
   context "shared examples" do
@@ -111,104 +95,99 @@ RSpec.describe AAPB::BatchIngest::PBCoreXMLInstantiationReset, reset_data: false
     it_behaves_like "a Hyrax::BatchIngest::BatchItemIngester"
   end
 
-
-  # Before each example in this spec:
-  # 1. Ingest batch_item_1 with PBCoreXMLItemIngester
-  # 2. Ingest batch_item_2 with PBCoreXMLInstantiationResetIngester
-  before do
-    AAPB::BatchIngest::PBCoreXMLItemIngester.new(batch_items.first).ingest
-    # Fetch the AssetResource as it was first ingested. This represents an
-    # "original" state of an AssetResource prior to running the
-    # PBCoreXMLInstantiationReset ingester, which we can use for comparison
-    @orig_asset_resource = AssetResource.find(pbcore_identifier.value).dup
-    AAPB::BatchIngest::PBCoreXMLInstantiationReset.new(batch_items.last).ingest
-  end
-
   describe '#ingest' do
     subject { described_class.new(batch_items.last).ingest }
 
     context 'when the Asset is not in AMS' do
-      it 'raises an exception with a useful message' do
-        expect { subject }.to raise_error('REPLACE WITH EXPECTED MESSAGE')
+      it 'raises a Valkyrie::Persistence::ObjectNotFoundError error' do
+        expect { subject }.to raise_error Valkyrie::Persistence::ObjectNotFoundError
       end
     end
 
     context 'when the Asset exists in AMS' do
-      context 'and when there is an error in the instantiations being reset' do
-        it 'raises an exception with a useful message' do
-          expect { subject }.to raise_error('REPLACE WITH VALIDATION MESSAGE')
+
+      # Before each example in this spec:
+      # 1. Ingest batch_item_1 with PBCoreXMLItemIngester
+      # 2. Ingest batch_item_2 with PBCoreXMLInstantiationResetIngester
+      before do
+        AAPB::BatchIngest::PBCoreXMLItemIngester.new(batch_items.first).ingest
+        # Fetch the AssetResource as it was first ingested. This represents an
+        # "original" state of an AssetResource prior to running the
+        # PBCoreXMLInstantiationReset ingester, which we can use for comparison
+        @orig_asset_resource = AssetResource.find(pbcore_identifier.value).dup
+        AAPB::BatchIngest::PBCoreXMLInstantiationReset.new(batch_items.last).ingest
+      end
+
+      let(:fetched_asset_resource) { AssetResource.find(pbcore_identifier.value) }
+
+      let(:asset_property_methods) do
+        [
+          :bulkrax_identifier,
+          :asset_types,
+          :genre,
+          :date,
+          :broadcast_date,
+          :created_date,
+          :copyright_date,
+          :episode_number,
+          :spatial_coverage,
+          :temporal_coverage,
+          :audience_level,
+          :audience_rating,
+          :annotation,
+          :rights_summary,
+          :rights_link,
+          :local_identifier,
+          :pbs_nola_code,
+          :eidr_id,
+          :topics,
+          :subject,
+          :program_title,
+          :episode_title,
+          :segment_title,
+          :raw_footage_title,
+          :promo_title,
+          :clip_title,
+          :program_description,
+          :episode_description,
+          :segment_description,
+          :raw_footage_description,
+          :promo_description,
+          :clip_description,
+          :producing_organization,
+          :admin_data_gid,
+          :series_title,
+          :series_description,
+          :intended_children_count,
+          :validation_status_for_aapb
+        ]
+      end
+
+      it 'does not raise an exception' do
+        expect { subject }.not_to raise_error
+      end
+
+      # HAPPY PATH SPEC! This is the main desired outcome.
+      it 'sets the instantiations to the new values' do
+        expect(actual_inst_local_ids).to eq expected_inst_local_ids
+      end
+
+      it 'does not udpate Admin Data' do
+        expect(fetched_asset_resource.admin_data).to eq @orig_asset_resource.admin_data
+      end
+
+      it 'does not update Annotations' do
+        expect(fetched_asset_resource.annotations).to eq @orig_asset_resource.annotations
+      end
+
+      it 'does not update the Asset attributes' do
+        asset_property_methods.each do |asset_property_method|
+          expect(fetched_asset_resource.send(asset_property_method)).to eq @orig_asset_resource.send(asset_property_method)
         end
       end
 
-      context 'and when there is no error in the instantiations being reset' do
-        let(:asset_property_methods) do
-          [
-            :bulkrax_identifier,
-            :asset_types,
-            :genre,
-            :date,
-            :broadcast_date,
-            :created_date,
-            :copyright_date,
-            :episode_number,
-            :spatial_coverage,
-            :temporal_coverage,
-            :audience_level,
-            :audience_rating,
-            :annotation,
-            :rights_summary,
-            :rights_link,
-            :local_identifier,
-            :pbs_nola_code,
-            :eidr_id,
-            :topics,
-            :subject,
-            :program_title,
-            :episode_title,
-            :segment_title,
-            :raw_footage_title,
-            :promo_title,
-            :clip_title,
-            :program_description,
-            :episode_description,
-            :segment_description,
-            :raw_footage_description,
-            :promo_description,
-            :clip_description,
-            :producing_organization,
-            :admin_data_gid,
-            :series_title,
-            :series_description,
-            :intended_children_count,
-            :validation_status_for_aapb
-          ]
-        end
-
-        it 'does not raise an exception' do
-          expect { subject }.not_to raise_error
-        end
-
-        it 'sets the instantiations to the new values', :focus do
-          expect(actual_instantiations).to eq expected_instantiations
-        end
-
-        it 'does not udpate Admin Data' do
-          expect(fetched_asset_resource.admin_data).to eq @orig_asset_resource.admin_data
-        end
-
-        it 'does not update Annotations' do
-          expect(fetched_asset_resource.annotations).to eq @orig_asset_resource.annotations
-        end
-
-        it 'does not update the Asset attributes' do
-          asset_property_methods.each do |asset_property_method|
-            expect(fetched_asset_resource.send(asset_property_method)).to eq @orig_asset_resource.send(asset_property_method)
-          end
-        end
-
-        it 'does not update the Contributions' do
-          expect(fetched_asset_resource.contributions).to eq @orig_asset_resource.contributions
-        end
+      it 'does not update the Contributions' do
+        expect(fetched_asset_resource.contributions).to eq @orig_asset_resource.contributions
       end
     end
   end
