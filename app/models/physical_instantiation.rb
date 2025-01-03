0566b77d8436f1a4810059dab09bb94cef6e9abd
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-01-03 15:04:17 -0500

    remove media type as required presence

--- app/models/physical_instantiation.rb
+++ app/models/physical_instantiation.rb
@@ -14,7 +14,6 @@ class PhysicalInstantiation < ActiveFedora::Base
 
   validates :format, presence: { message: 'Your work must have a format.' }
   validates :location, presence: { message: 'Your work must have a location.' }
-  validates :media_type, presence: { message: 'Your work must have a media type.' }
   validates :duration, format: { with: AMS::TimeCodeService.regex, allow_blank: true, message: "Invalid format for duration. Use HH:MM:SS, H:MM:SS, MM:SS, or M:SS" }
   # Custom validation block for time_start multi-valued field.
   validate do |physical_instantiation|
