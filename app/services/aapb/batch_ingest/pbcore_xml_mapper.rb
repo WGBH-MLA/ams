b44a37f23d910a743a06538a58ab9caa880917e6
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-01-07 15:50:38 -0500

    reverting to allow nil for format

--- app/services/aapb/batch_ingest/pbcore_xml_mapper.rb
+++ app/services/aapb/batch_ingest/pbcore_xml_mapper.rb
@@ -171,7 +171,7 @@ module AAPB
 
       def physical_instantiation_resource_attributes
         @physical_instantiation_resource_attributes ||= instantiation_attributes.tap do |attrs|
-          attrs[:format] = pbcore.physical.value if pbcore.physical.value.present?
+          attrs[:format] = pbcore.physical.value || nil
         end
       end
 
