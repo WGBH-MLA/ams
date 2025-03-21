4d4a2ef63ded43ec0f334c51e10c039839f6ce02
Shana Moore <shana.lavina.moore@gmail.com>
2025-03-21 13:56:38 -0700

    override clone behavior; allows user to add another blank
    When a user removes all contributor, this allows the user to re add another.
    
    Before, because hydra-editor was looking for something to clone, when all were removed it broke and wouldn't let a user add anything. They'd have to refresh the page to get another row back.

--- app/assets/javascripts/work_actions/child_contributors_input.js
+++ app/assets/javascripts/work_actions/child_contributors_input.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  var $template = $('.form-group.child_contributors .listing li.field-wrapper').first().clone();
+  
+  $('.form-group.child_contributors .has-warning, .form-group.child_contributors .message.has-warning').remove();
+  
+  $('.form-group.child_contributors .add').off('click');
+  
   $('.form-group.child_contributors .remove').on('click', function(e) {
     var $wrapper = $(this).closest('li.field-wrapper');
     var contributorId = $wrapper.find('input[name$="[id]"]').val();
@@ -6,23 +12,47 @@ $(document).ready(function() {
     if (contributorId && contributorId !== '') {
       var $form = $(this).closest('form');
 
-      // Create a hidden input for the ID
       var idInput = $('<input>')
         .attr('type', 'hidden')
         .attr('name', 'asset_resource[contributors][][id]')
         .val(contributorId);
 
-      // Create a hidden input for _destroy flag
       var destroyInput = $('<input>')
         .attr('type', 'hidden')
         .attr('name', 'asset_resource[contributors][][_destroy]')
         .val('true');
 
-      // Add them to the form
       $form.append(idInput);
       $form.append(destroyInput);
     }
 
     $wrapper.remove();
+  });
+
+  // Override the hydra-editor's warning display function
+  if (typeof FieldManager !== 'undefined') {
+    FieldManager.prototype.displayEmptyWarning = function() {
+      // Do nothing - this prevents the warning from being displayed
+    };
+  }
+
+  $('.form-group.child_contributors .add').on('click', function(e) {
+    e.preventDefault(); // Prevent default hydra-editor behavior
+    e.stopPropagation();
+    
+    $('.form-group.child_contributors .has-warning, .form-group.child_contributors .message.has-warning').remove();
+    
+    var $listing = $(this).closest('.form-group.child_contributors').find('.listing');
+    
+    var $templateToUse = $listing.children('li.field-wrapper').last().length > 0 
+      ? $listing.children('li.field-wrapper').last()
+      : $template;
+    
+    var $newField = $templateToUse.clone();
+    $newField.find('input').val('').removeAttr('required');
+    
+    $listing.append($newField);
+    
+    $newField.find('input').first().focus();
   });
 });
