54cacf813ef71685b0e1f0fcfcc47c24928806d7
braydonjustice <braydonjustice@gmail.com>
2025-04-11 07:26:25 -0700

    adds a check for Instantiations, excludes digital, and adds a placeholder text for format if it is missing.

--- app/services/aapb/batch_ingest/csv_item_ingester.rb
+++ app/services/aapb/batch_ingest/csv_item_ingester.rb
@@ -77,6 +77,10 @@ module AAPB
 
           attributes["admin_set_id"] = @batch_item.batch.admin_set_id
 
+          if node.object_class.include?("Instantiation") && node.object_class != "DigitalInstantiation"
+            attributes["format"] = attributes["format"].present? ? attributes["format"] : 'Instantiation format not provided'
+          end
+
           if ingest_type == "new"
             model_object = object_class_for(node.object_class).new
 
