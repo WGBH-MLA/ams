6e07439ee8ac493c418d941ed7d6d94b40f66f15
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-05-12 17:06:26 -0400

    adds a no value option

--- spec/factories/annotation.rb
+++ spec/factories/annotation.rb
@@ -3,13 +3,19 @@ FactoryBot.define do
     ref { Faker::Internet.url }
     value { Faker::Company.bs }
     admin_data { nil }
-    annotation_type { nil } # Set to nil by default
+    annotation_type { nil } # Default to nil unless overridden
     source { nil }
     annotation { nil }
     version { nil }
 
+    # Trait for annotations with a specific annotation_type
     trait :with_annotation_type do
       annotation_type { AnnotationTypesService.new.select_all_options.to_h.values.sample }
+    end
+
+    # Trait for annotations without a value
+    trait :no_value do
+      value { nil }
     end
   end
 end
