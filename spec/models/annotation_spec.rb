f3dfb1cc2974e32d589a2b9ed6b9fb2657620177
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-05-12 16:42:46 -0400

    produce tests with nil annotationtypes

--- spec/models/annotation_spec.rb
+++ spec/models/annotation_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Annotation, type: :model do
-
   describe "attributes" do
     context "without AdminData" do
       let(:annotation) { FactoryBot.build(:annotation) }
@@ -21,26 +20,19 @@ RSpec.describe Annotation, type: :model do
         end
       end
 
-      context "and no value" do
-        let(:annotation) { FactoryBot.build(:annotation, :no_value, admin_data: admin_data) }
+      context "with nil annotation_type" do
+        let(:annotation) { FactoryBot.build(:annotation, admin_data: admin_data, annotation_type: nil) }
 
-        it "is invalid" do
-          expect(annotation.valid?).to be false
+        it "is valid when annotation_type is nil" do
+          expect(annotation.valid?).to be true
         end
       end
 
-      context "a relationship is established" do
-        let!(:annotation) { FactoryBot.create(:annotation, admin_data: admin_data) }
+      context "with annotation_type present" do
+        let(:annotation) { FactoryBot.build(:annotation, admin_data: admin_data, annotation_type: "example_type") }
 
-        it "can access annotations from AdminData" do
-          expect(admin_data.reload.annotations.first).to eq(annotation)
-        end
-      end
-
-      context "has a Supplemental Material annotation_type and no ref attribute" do
-        let!(:annotation) { FactoryBot.build(:annotation, annotation_type: "supplemental_material", ref: nil, admin_data: admin_data) }
-        it "is invalid without a ref attribute" do
-          expect(annotation.valid?).to be false
+        it "is valid when annotation_type is present" do
+          expect(annotation.valid?).to be true
         end
       end
     end
