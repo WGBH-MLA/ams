de85623614eb18d757bac940d3e2264aa547270f
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-05-12 16:38:28 -0400

    create test annotations without types

--- spec/factories/annotation.rb
+++ spec/factories/annotation.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
     ref { Faker::Internet.url }
     value { Faker::Company.bs }
     admin_data { nil }
-    annotation_type { AnnotationTypesService.new.select_all_options.to_h.values.sample }
+    annotation_type { nil } # Set to nil by default
     source { nil }
     annotation { nil }
     version { nil }
 
-    trait :no_value do
-      value { nil }
+    trait :with_annotation_type do
+      annotation_type { AnnotationTypesService.new.select_all_options.to_h.values.sample }
     end
   end
 end
