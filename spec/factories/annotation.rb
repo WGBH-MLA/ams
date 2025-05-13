c67aba37f8d1f424f6496dca7cd8f1224bee3637
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-05-13 13:11:45 -0400

    Update annotation.rb

--- spec/factories/annotation.rb
+++ spec/factories/annotation.rb
@@ -3,19 +3,18 @@ FactoryBot.define do
     ref { Faker::Internet.url }
     value { Faker::Company.bs }
     admin_data { nil }
-    annotation_type { nil } # Default to nil unless overridden
+    annotation_type { AnnotationTypesService.new.select_all_options.to_h.values.sample }
     source { nil }
     annotation { nil }
     version { nil }
 
-    # Trait for annotations with a specific annotation_type
-    trait :with_annotation_type do
-      annotation_type { AnnotationTypesService.new.select_all_options.to_h.values.sample }
-    end
 
-    # Trait for annotations without a value
     trait :no_value do
       value { nil }
     end
+
+    train :no_annotation_type do
+      annotation_type { nil }
   end
 end
+  
