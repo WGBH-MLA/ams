84a22ff2b4b15f33b52ff041032329c4730e2c49
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-05-13 13:20:44 -0400

    Update annotation_spec.rb

--- spec/models/annotation_spec.rb
+++ spec/models/annotation_spec.rb
@@ -1,40 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Annotation, type: :model do
-  describe "attributes" do
-    context "without AdminData" do
-      let(:annotation) { FactoryBot.build(:annotation) }
-      it "is invalid" do
-        expect(annotation.valid?).to be false
-      end
+    describe "attributes" do
+        context "without AdminData" do
+            let(:annotation) { FactoryBot.build(:annotation) }
+            it "is invalid" do
+                expect(annotation.valid?).to be false
+            end
+        end
+
+        context "with AdminData" do
+            let(:admin_data) { FactoryBot.create(:admin_data) }
+
+            context "and all data" do
+                let(:annotation) { FactoryBot.build(:annotation, admin_data: admin_data) }
+
+                it "is valid" do
+                    expect(annotation.valid?).to be true
+                end
+            end
+
+            context "with nil annotation_type" do
+                let(:annotation) { FactoryBot.build(:annotation, :no_annotation_type, admin_data: admin_data) }
+
+                it "is valid without an annotation_type" do
+                    expect(annotation.annotation_type).to be_nil
+                    expect(annotation.valid?).to be true
+                end
+            end
+        end
     end
-
-    context "with AdminData" do
-      let(:admin_data) { FactoryBot.create(:admin_data) }
-
-      context "and all data" do
-        let(:annotation) { FactoryBot.build(:annotation, admin_data: admin_data) }
-
-        it "is valid" do
-          expect(annotation.valid?).to be true
-        end
-      end
-
-      context "with nil annotation_type" do
-        let(:annotation) { FactoryBot.build(:annotation, admin_data: admin_data, annotation_type: nil) }
-
-        it "is valid when annotation_type is nil" do
-          expect(annotation.valid?).to be true
-        end
-      end
-
-      context "with annotation_type present" do
-        let(:annotation) { FactoryBot.build(:annotation, :with_annotation_type) }
-
-        it "is valid when annotation_type is present" do
-          expect(annotation.valid?).to be true
-        end
-      end
-    end
-  end
 end
