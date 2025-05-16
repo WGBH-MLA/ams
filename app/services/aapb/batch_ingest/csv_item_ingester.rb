0196be6cc160a786a199b5e125d49d07b1437d54
braydonjustice <braydonjustice@gmail.com>
2025-05-16 09:58:20 -0700

    ingesters now do two things around missing PI format. If heading/tag exists but is empty, an 'Unavailable' placeholder is added, if the heading/tag does not exist it just doesn't add it to the metadata.

--- app/services/aapb/batch_ingest/csv_item_ingester.rb
+++ app/services/aapb/batch_ingest/csv_item_ingester.rb
@@ -78,7 +78,7 @@ module AAPB
           attributes["admin_set_id"] = @batch_item.batch.admin_set_id
 
           unless %w[Asset DigitalInstantiation EssenceTrack].include?(node.object_class)
-            attributes["format"] = attributes["format"].present? ? attributes["format"] : '   '
+            attributes["format"] = attributes["format"].presence || 'Unavailable' if with_data.key?("format")
           end
 
           if ingest_type == "new"
