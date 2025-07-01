dc36efab5995029d37bd26e6b2991f893d5e644f
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2025-07-01 12:09:28 -0400

    changes priority desc list

--- spec/models/solr_document_spec.rb
+++ spec/models/solr_document_spec.rb
@@ -44,8 +44,7 @@ describe SolrDocument do
 
   describe '#display_description' do
     let(:description_type_preferred_order) do
-      %i( rundown_description raw_footage_description segment_description clip_description
-          promo_description episode_description program_description )
+      %i( raw_footage_description segment_description clip_description rundown_description promo_description episode_description program_description )
     end
 
     it 'returns the most preferred description type' do
