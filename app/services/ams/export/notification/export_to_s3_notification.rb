2b54d5b723385b682adfa2374fa526b67de0e0d8
braydonjustice <braydonjustice@gmail.com>
2025-10-12 22:20:58 -0700

    force mailers to use sidekiq queue to send mail.

--- app/services/ams/export/notification/export_to_s3_notification.rb
+++ app/services/ams/export/notification/export_to_s3_notification.rb
@@ -4,11 +4,11 @@ module AMS
       class ExportToS3Notification < Base
         def send_failure(error_message: nil)
           mail_params[:error_message] = error_message
-          mailer.export_to_s3_failed.deliver_now
+          mailer.export_to_s3_failed.deliver_later
         end
 
         def send_success
-          mailer.export_to_s3_succeeded.deliver_now
+          mailer.export_to_s3_succeeded.deliver_later
         end
       end
     end
