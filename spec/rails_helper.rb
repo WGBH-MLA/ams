6b70f597cb81c8a1204a9cd6b5d48c5452e8a5e8
sephirothkod <braydon.justice@1268456bcltd.ca>
2025-12-11 04:47:16 -0800

    updated location of BULKRAX_ENABLED definition since it wasn't declared early enough

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,11 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
+# Bulkrax feature flag - must be defined before FactoryBot loads factories
+# Set to false to skip Bulkrax-related tests
+BULKRAX_ENABLED = ENV.fetch('SETTINGS__BULKRAX__ENABLED', 'true').downcase == 'true'
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -144,10 +149,6 @@ end
 # Uncomment this to help debug ChromeDriver (or other web driver) issues on
 # Travis or development.
 # Webdrivers.logger.level = :DEBUG
-
-# Bulkrax feature flag
-# Set to false to skip Bulkrax-related tests
-BULKRAX_ENABLED = ENV.fetch('SETTINGS__BULKRAX__ENABLED', 'true').downcase == 'true'
 
 require 'valkyrie'
 Valkyrie::MetadataAdapter.register(Valkyrie::Persistence::Memory::MetadataAdapter.new, :test_adapter)
