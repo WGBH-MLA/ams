ccdb53081a53fc60026d33aa11bf5e2f4221746a
Braydon Justice <sephirothkod@users.noreply.github.com>
2025-12-11 04:16:05 -0800

    revert s3 email delivery from async to sync

--- app/services/ams/export/notification/export_to_s3_notification.rb
+++ app/services/ams/export/notification/export_to_s3_notification.rb
@@ -4,11 +4,11 @@ module AMS
       class ExportToS3Notification < Base
         def send_failure(error_message: nil)
           mail_params[:error_message] = error_message
-          mailer.export_to_s3_failed.deliver_later
+          mailer.export_to_s3_failed.deliver_now
         end
 
         def send_success
-          mailer.export_to_s3_succeeded.deliver_later
+          mailer.export_to_s3_succeeded.deliver_now
         end
       end
     end
