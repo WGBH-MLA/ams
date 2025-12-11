72702ce048edbaf1f8944c78a1fc232a34bec0b2
Braydon Justice <sephirothkod@users.noreply.github.com>
2025-12-11 04:16:37 -0800

    revert aapb email delivery from async to sync

--- app/services/ams/export/notification/push_to_aapb_notification.rb
+++ app/services/ams/export/notification/push_to_aapb_notification.rb
@@ -4,11 +4,11 @@ module AMS
       class PushToAAPBNotification < Base
         def send_failure(error_message: nil)
           mail_params[:error_message] = error_message
-          mailer.push_to_aapb_failed.deliver_later
+          mailer.push_to_aapb_failed.deliver_now
         end
 
         def send_success
-          mailer.push_to_aapb_succeeded.deliver_later
+          mailer.push_to_aapb_succeeded.deliver_now
         end
       end
     end
