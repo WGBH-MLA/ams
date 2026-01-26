ccb8a0bc0ea6aa70eb25eb210de85f3e877e047d
sephirothkod <braydon.justice@1268456bcltd.ca>
2026-01-26 00:44:48 -0800

    added proper indexing and saving for valkyrie objects

--- spec/support/factory_bot_valkyrie.rb
+++ spec/support/factory_bot_valkyrie.rb
@@ -12,15 +12,23 @@ module FactoryBotValkyrieStrategy
 
       # Use Valkyrie persister for Valkyrie resources instead of calling save!
       if instance.is_a?(Valkyrie::Resource)
-        Hyrax.persister.save(resource: instance)
-      else
-        # Fall back to default behavior for non-Valkyrie objects
-        if @to_create
-          @to_create.call(instance, evaluation)
-        else
-          instance.save!
-        end
+        # Save to persistence layer
+        result = Hyrax.persister.save(resource: instance)
+        # Index to Solr (this is what ArResource#save also does)
+        Hyrax.index_adapter.save(resource: result)
+        # Update the instance with the persisted ID
+        instance.id = result.id if result.respond_to?(:id)
+      elsif @to_create
+        # Use custom to_create callback if defined
+        @to_create.call(instance, evaluation)
+      elsif instance.respond_to?(:save!)
+        # Default FactoryBot behavior
+        instance.save!
+      elsif instance.respond_to?(:save)
+        # Try save without bang
+        instance.save
       end
+      # If none of the above, the object doesn't need persisting (e.g., PBCore::DescriptionDocument)
 
       evaluation.notify(:after_create, instance)
     end
