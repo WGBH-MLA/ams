a17c345296ffd90a0a48eb05108f9fb73dbc44d0
sephirothkod <braydon.justice@1268456bcltd.ca>
2026-01-27 02:11:59 -0800

    patched evaluator to account for create method mismatch on save

--- spec/support/factory_bot_valkyrie.rb
+++ spec/support/factory_bot_valkyrie.rb
@@ -1,56 +1,93 @@
 # frozen_string_literal: true
 
-# Configure FactoryBot to use Valkyrie persister for resource classes
-# This fixes Ruby 3.0+ compatibility issues with Hyrax::ArResource#save
-# which doesn't accept positional arguments but FactoryBot may pass them
+# Fix Ruby 3.0+ compatibility with FactoryBot and Hyrax::ArResource#save
+#
+# Problem: Hyrax::ArResource#save only accepts keyword arguments, but FactoryBot
+# may pass positional arguments, causing ArgumentError in Ruby 3.0+
+#
+# Solution: Multiple-layered approach to intercept save calls
 
-# Patch FactoryBot's evaluator to handle Valkyrie resource save calls
-module FactoryBotEvaluatorValkyriefix
+# Define the fix module that strips positional arguments
+module HyraxSaveArgumentFix
+  def save(*_args, **kwargs)
+    super(**kwargs)
+  end
+
+  def save!(*_args, **kwargs)
+    super(**kwargs)
+  end
+end
+
+# List of resource classes to patch
+VALKYRIE_RESOURCE_CLASSES = %w[
+  AssetResource
+  DigitalInstantiationResource
+  PhysicalInstantiationResource
+  EssenceTrackResource
+  ContributionResource
+].freeze
+
+# Apply patches to all resource classes
+VALKYRIE_RESOURCE_CLASSES.each do |class_name|
+  begin
+    klass = class_name.constantize
+    unless klass.ancestors.include?(HyraxSaveArgumentFix)
+      klass.prepend(HyraxSaveArgumentFix)
+    end
+  rescue NameError
+    # Class not loaded yet
+  end
+end
+
+# Patch FactoryBot::Evaluator#method_missing to handle Hyrax::ArResource methods
+# that only accept keyword arguments (save, save!, create, create!, update, update!)
+module FactoryBotEvaluatorSaveFix
+  # Methods in Hyrax::ArResource that only accept keyword arguments
+  KEYWORD_ONLY_METHODS = [:save, :save!, :create, :create!, :update, :update!].freeze
+
   def method_missing(method_name, *args, &block)
-    # Intercept save/save! calls for Valkyrie resources and strip positional args
-    if %i[save save!].include?(method_name) && @instance.is_a?(Valkyrie::Resource)
-      # Call save without positional arguments, only keyword args
-      kwargs = args.last.is_a?(Hash) ? args.last : {}
-      @instance.public_send(method_name, **kwargs)
+    method_sym = method_name.to_sym
+
+    if @instance.respond_to?(method_name)
+      # For Hyrax::ArResource methods that only accept kwargs, strip positional args
+      # (create/update are aliased to save in ArResource)
+      if KEYWORD_ONLY_METHODS.include?(method_sym)
+        return method_sym.to_s.end_with?('!') ? @instance.save! : @instance.save
+      end
+      @instance.public_send(method_name, *args, &block)
     else
-      super
+      FactoryBot::SyntaxRunner.new.send(method_name, *args, &block)
     end
   end
 end
 
-# Apply the evaluator patch
-FactoryBot::Evaluator.prepend(FactoryBotEvaluatorValkyriefix)
+# Apply to FactoryBot::Evaluator
+if defined?(FactoryBot::Evaluator)
+  FactoryBot::Evaluator.prepend(FactoryBotEvaluatorSaveFix)
+end
 
-# Also patch the FactoryBot create strategy
+# Patch FactoryBot's create strategy to use Valkyrie persister directly
+# This ensures Valkyrie resources are saved correctly
 module FactoryBotValkyrieStrategy
   def result(evaluation)
     evaluation.object.tap do |instance|
       evaluation.notify(:after_build, instance)
 
-      # Use Valkyrie persister for Valkyrie resources instead of calling save!
-      if instance.is_a?(Valkyrie::Resource)
-        # Save to persistence layer
+      if defined?(Valkyrie::Resource) && instance.is_a?(Valkyrie::Resource)
         result = Hyrax.persister.save(resource: instance)
-        # Index to Solr (this is what ArResource#save also does)
         Hyrax.index_adapter.save(resource: result)
-        # Update the instance with the persisted ID
         instance.id = result.id if result.respond_to?(:id)
       elsif @to_create
-        # Use custom to_create callback if defined
         @to_create.call(instance, evaluation)
       elsif instance.respond_to?(:save!)
-        # Default FactoryBot behavior
         instance.save!
       elsif instance.respond_to?(:save)
-        # Try save without bang
         instance.save
       end
-      # If none of the above, the object doesn't need persisting (e.g., PBCore::DescriptionDocument)
 
       evaluation.notify(:after_create, instance)
     end
   end
 end
 
-# Apply the patch to FactoryBot's create strategy
 FactoryBot::Strategy::Create.prepend(FactoryBotValkyrieStrategy)
