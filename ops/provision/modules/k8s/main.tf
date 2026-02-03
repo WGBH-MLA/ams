07799176b396bff8bd1d7e940a6c07b248b72997
Andrew Myers <afredmyers@gmail.com>
2026-02-03 12:50:22 -0500

    Removes Rancher from required providers from TF template
    Closes #1122

--- ops/provision/modules/k8s/main.tf
+++ ops/provision/modules/k8s/main.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    rancher2 = {
-      source = "rancher/rancher2"
-      version = "1.11.0"
-    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
