a1557038853ca76cd315e47d0a41755752b3f8ca
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2026-04-27 11:14:09 -0400

    sets admin/user credentials in env for local testing, removed seeding for test users on prod/demo

---
+++ db/seeds/development.rb
@@ -0,0 +1,10 @@
+user = User.find_or_create_by!(email: ENV['DEV_ADMIN_EMAIL']) do |u|
+  u.password = ENV['DEV_ADMIN_PASSWORD']
+end
+
+admin_role = Role.find_or_create_by(name: 'admin')
+aapb_admin_role = Role.find_or_create_by(name: 'aapb-admin')
+user.roles << admin_role unless user.roles.include?(admin_role)
+user.roles << aapb_admin_role unless user.roles.include?(aapb_admin_role)
+
+Hyrax::AdminSetCreateService.find_or_create_default_admin_set
