9282d42d1d7819cc4a805d53c6faa89c81feea11
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2026-04-27 12:23:37 -0400

    guarding against nil in seeds file so github actions can run without error.

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,10 +1,12 @@
-user = User.find_or_create_by!(email: ENV['DEV_ADMIN_EMAIL']) do |u|
-  u.password = ENV['DEV_ADMIN_PASSWORD']
-end
+if ENV['DEV_ADMIN_EMAIL'].present? && ENV['DEV_ADMIN_PASSWORD'].present?
+  user = User.find_or_create_by!(email: ENV['DEV_ADMIN_EMAIL']) do |u|
+    u.password = ENV['DEV_ADMIN_PASSWORD']
+  end
 
-admin_role = Role.find_or_create_by(name: 'admin')
-aapb_admin_role = Role.find_or_create_by(name: 'aapb-admin')
-user.roles << admin_role unless user.roles.include?(admin_role)
-user.roles << aapb_admin_role unless user.roles.include?(aapb_admin_role)
+  admin_role = Role.find_or_create_by(name: 'admin')
+  aapb_admin_role = Role.find_or_create_by(name: 'aapb-admin')
+  user.roles << admin_role unless user.roles.include?(admin_role)
+  user.roles << aapb_admin_role unless user.roles.include?(aapb_admin_role)
+end
 
 Hyrax::AdminSetCreateService.find_or_create_default_admin_set
