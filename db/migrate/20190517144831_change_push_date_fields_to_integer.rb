312b6ecac5ca6dab3d902577ec43cb898e246b82
Harpo Harbert <ryan_harbert@wgbh.org>
2026-06-30 16:02:42 -0700

    Try: integer migrations fix

--- db/migrate/20190517144831_change_push_date_fields_to_integer.rb
+++ db/migrate/20190517144831_change_push_date_fields_to_integer.rb
@@ -1,6 +1,11 @@
 class ChangePushDateFieldsToInteger < ActiveRecord::Migration[5.1]
-  def change
-    change_column :admin_data, :last_pushed, :integer
-    change_column :admin_data, :last_updated, :integer
+  def up
+    change_column :admin_data, :last_pushed, :integer, using: 'last_pushed::integer'
+    change_column :admin_data, :last_updated, :integer, using: 'last_updated::integer'
+  end
+
+  def down
+    change_column :admin_data, :last_pushed, :string
+    change_column :admin_data, :last_updated, :string
   end
 end
