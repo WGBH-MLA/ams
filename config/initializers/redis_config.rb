cc78e9a7075ad0f381c8a447ca99798811978a26
Harpo Harbert <ryan_harbert@wgbh.org>
2026-07-07 13:36:56 -0700

    Try: redis config without thread_safe: true

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,7 +1,7 @@
 require 'redis'
 config = YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
 Hyrax.config.redis_connection = begin
-                                  Redis.new(config.merge(thread_safe: true))
+                                  Redis.new
                                 rescue
                                   nil
                                 end
