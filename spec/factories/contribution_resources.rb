8e0a0aef6b337d47e9682517334808edb984146c
Andrew Myers <afredmyers@gmail.com>
2026-07-28 16:18:22 -0400

    Adds metadata fields to ContributionResource model and factory

--- spec/factories/contribution_resources.rb
+++ spec/factories/contribution_resources.rb
@@ -2,8 +2,17 @@ FactoryBot.define do
   factory :contribution_resource do
     contributor  { ["Test Contributor"] }
     contributor_role  { "Actor" }
+    contributor_role_annotation { "Test contributor role annotation" }
     portrayal  { "Test portrayal" }
     affiliation  { "Test affiliation" }
-    annotation  { "Test annotation" }
+    affiliation_source { "Test affiliation source" }
+    affiliation_ref { "https://test-affiliation-ref.com" }
+    affiliation_version { "Test affiliation version" }
+    affiliation_annotation { "Test affiliation annotation" }
+    source { "Test source" }
+    annotation { "Test annotation" }
+    start_time { "00:00:27" }
+    end_time { "00:03:30" }
+    time_annotation { "Test time annotation" }
   end
 end
