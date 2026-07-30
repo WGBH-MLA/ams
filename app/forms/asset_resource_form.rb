71311b172b3a4751e6787d4f5ff6239e19d2ff88
Andrew Myers <afredmyers@gmail.com>
2026-07-30 13:10:24 -0400

    Formats big ruby array to vertical style

--- app/forms/asset_resource_form.rb
+++ app/forms/asset_resource_form.rb
@@ -89,7 +89,18 @@ class AssetResourceForm < Hyrax::Forms::ResourceForm(AssetResource)
     child_contributions = []
     model.members.to_a.each do |member|
       if( member.internal_resource == 'Contribution' )
-        child_contributions << [member.id, member.contributor_role, member.contributor.first , member.portrayal, member.affiliation, member.annotation]
+        child_contributions << [
+          member.id,
+          member.contributor_role,
+          member.contributor.first ,
+          member.contributor_role_annotation,
+          member.portrayal,
+          member.affiliation,
+          member.affiliation_annotation,
+          member.annotation,
+          member.start_time,
+          member.end_time,
+          member.time_annotation]
       end
     end
     child_contributions
