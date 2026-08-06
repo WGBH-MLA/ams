2b3b53afc5f531352e719a588b824e21b5feb513
Andrew Myers <afredmyers@gmail.com>
2026-08-06 13:10:15 -0400

    Corrects start and end time types. This still is still causing failure to save contributors due to the type, for now still trying to get it to work but will convert back to string if it's too hard.

--- app/forms/asset_resource_form.rb
+++ app/forms/asset_resource_form.rb
@@ -5,6 +5,8 @@
 #
 # @see https://github.com/samvera/hyrax/wiki/Hyrax-Valkyrie-Usage-Guide#forms
 # @see https://github.com/samvera/valkyrie/wiki/ChangeSets-and-Dirty-Tracking
+
+
 class AssetResourceForm < Hyrax::Forms::ResourceForm(AssetResource)
   include Hyrax::FormFields(:basic_metadata)
   include Hyrax::FormFields(:asset_resource)
@@ -15,6 +17,11 @@ class AssetResourceForm < Hyrax::Forms::ResourceForm(AssetResource)
   validates :date, date: { allow_blank: true }
   validates :broadcast_date, date: { allow_blank: true }
   validates :copyright_date, date: { allow_blank: true }
+
+  # NOTE: the `child_contributors: true` tells Rails to use the
+  # ChildContributorsValidator class to validate the child_contributors
+  # property. See app/validators/child_contributors_validator.rb.
+  validates :child_contributors, contributors_metadata: true
 
   attr_accessor :controller, :current_ability
 
