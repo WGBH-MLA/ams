070b60fcb37b3ff7b77a698f57f840aba0940909
Andrew Myers <afredmyers@gmail.com>
2026-08-14 15:01:28 -0400

    Removes before_action hook to convert contributor start and end times from empty strings to floats.
    however I could not find how to get solr to avoid attempting to save empty strings. so we had to settle
    on just storing strings. If we ever get to indexing them as floats again, this code/strategy was on the way
    to working but had to abandon.

--- app/controllers/hyrax/asset_resources_controller.rb
+++ app/controllers/hyrax/asset_resources_controller.rb
@@ -17,9 +17,6 @@ module Hyrax
     self.work_form_service = Hyrax::FormFactory.new
     self.show_presenter = AssetResourcePresenter
 
-    before_action :convert_blank_floats_to_nil, only: [:create, :update]
-
-
     private
     # This extends functionality from
     # Hyrax::WorksControllerBehavior#additional_response_formats, adding a
@@ -27,17 +24,6 @@ module Hyrax
     def additional_response_formats(format)
       format.xml { render(plain: presenter.solr_document.export_as_pbcore) }
       super
-    end
-
-
-    # Removes blank start_time and end_time values from the contributors hash in the params.
-    # This is necessary because the form allows for empty values, but the model expects these fields to be either a valid float or nil.
-    def convert_blank_floats_to_nil
-      params.fetch("asset_resource", {})
-            .fetch("contributors", []).each do |contrib|
-              contrib["start_time"] = nil if contrib.fetch("start_time", "").blank?
-              contrib["end_time"] = nil if contrib.fetch("end_time", "").blank?
-            end
     end
   end
 end
