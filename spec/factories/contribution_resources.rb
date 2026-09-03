9076a23e109a9882f380d7d926f7d79a24fa2c76
Andrew Myers <afredmyers@gmail.com>
2026-09-03 15:58:16 -0400

    STASH WIP

--- spec/factories/contribution_resources.rb
+++ spec/factories/contribution_resources.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :contribution_resource do
-    contributor  { ["Test Contributor"] }
-    contributor_role  { "Actor" }
-    contributor_role_annotation { "Test contributor role annotation" }
-    portrayal  { "Test portrayal" }
-    affiliation  { "Test affiliation" }
-    affiliation_annotation { "Test affiliation annotation" }
-    source { "Test source" }
-    annotation { "Test annotation" }
-    start_time { "00:00:27" }
-    end_time { "00:03:30" }
-    time_annotation { "Test time annotation" }
+    sequence(:contributor)  { |n| ["Test Contributor #{n}"] }
+    sequence(:contributor_role)  { |n| "Actor #{n}" }
+    sequence(:contributor_role_annotation) { |n| "Test contributor role annotation #{n}" }
+    sequence(:portrayal)  { |n| "Test portrayal #{n}" }
+    sequence(:affiliation)  { |n| "Test affiliation #{n}" }
+    sequence(:affiliation_annotation) { |n| "Test affiliation annotation #{n}" }
+    sequence(:source) { |n| "Test source #{n}" }
+    sequence(:annotation) { |n| "Test annotation #{n}" }
+    sequence(:start_time) { |n| rand(n.to_f..(n+10).to_f).round(3).to_s }
+    sequence(:end_time) { |n| (start_time.to_f + rand(1.0..10.0)).round(3).to_s }
+    sequence(:time_annotation) { |n| "Test time annotation #{n}" }
   end
 end
--- spec/services/aapb/batch_ingest/csv_reader_spec.rb
+++ spec/services/aapb/batch_ingest/csv_reader_spec.rb
@@ -7,30 +7,101 @@ RSpec.describe AAPB::BatchIngest::CSVReader do
   context 'for ingest type aapb_csv_reader_1' do
     let(:ingest_type) { :aapb_csv_reader_1 }
 
-    describe '#batch_items' do
-      let(:batch_items) { subject.batch_items }
+    context ', when input CSV contains Contributor data' do
+      let(:csv) {
+        create(:csv, rows: csv_rows, headers: csv_headers)
+      }
 
-      context 'when input CSV contains rows with emtpy cells for Contributor' do
-
-        let(:csv) {
-          create( :csv,
-                  headers: ["Contribution", "Contribution.contributor" , "Contribution.contributor_role" , "Contribution.contributor" , "Contribution.contributor_role"],
-                  rows: [
-                    ["", "Patti Smith", "Everything", "Art Vandelay", "Exporter"],
-                    ["", "Steve", "Key grip", "", ""],
-                    ["", "", "", "", ""]
-                  ]
-          )
-        }
-
-        let(:contribution_data) { subject.batch_items.map { |batch_item|
-          JSON.parse(batch_item.source_data)['Contribution']
-        }}
-
-        it 'does not contain data for empty Contributions' do
-          expect(contribution_data[0]).to eq( [ { "contributor" => [ "Patti Smith", "Art Vandelay"], "contributor_role"=>"Exporter" } ] )
-          expect(contribution_data[1]).to eq( [ { "contributor" => [ "Steve" ], "contributor_role" => "Key grip" } ] )
-          expect(contribution_data[2]).to eq( [ { "contributor" => [] } ] )
+      let(:asset_headers) {
+        [
+          "Asset",
+          "Asset.program_title",
+          "Asset.program_description"
+        ]
+      }
+  
+      let(:contrib_headers) {
+        [
+          "Contribution",
+          "Contribution.contributor",
+          "Contribution.contributor_role",
+          "Contribution.contributor_role_annotation",
+          "Contribution.affiliation",
+          "Contribution.affiliation_annotation",
+          "Contribution.portrayal",
+          "Contribution.annotation",
+          "Contribution.start_time",
+          "Contribution.end_time",
+          "Contribution.time_annotation"
+        ]
+      }
+  
+      let(:csv_headers) {
+        [
+          asset_headers,
+          # Repeat the Contribution headers for the max number of Contributors we have in contrib_csv_rows
+          contrib_headers * contrib_csv_rows.map{ |row| row.count }.max
+        ].flatten
+      }
+  
+      # A list of attributes for an AssetResource model used for generating test CSV rows.
+      let(:asset_csv_rows) {
+        attributes_for_list(:asset_resource, 2)
+      }
+  
+      let(:contrib_csv_rows) {
+        asset_csv_rows.count.times.map do |i|
+          attributes_for_list(:contribution_resource, 2)
+        end
+      }
+  
+      # Uses built (not saved) AssetResource and ContributionResource instances to
+      # build CSV rows for a CSV file for testing the CSV ingest.
+      let(:csv_rows) {
+        asset_csv_rows.map.with_index do |ar, i|
+          [
+            "",
+            ar[:program_title].first,
+            ar[:program_description].first,
+            contrib_csv_rows[i].map do |contrib_vals|
+              [
+                "",
+                contrib_vals[:contributor].first,
+                contrib_vals[:contributor_role],
+                contrib_vals[:contributor_role_annotation],
+                contrib_vals[:affiliation],
+                contrib_vals[:affiliation_annotation],
+                contrib_vals[:portrayal],
+                contrib_vals[:annotation],
+                contrib_vals[:start_time],
+                contrib_vals[:end_time],
+                contrib_vals[:time_annotation]
+              ]
+            end
+          ].flatten
+        end      
+      }
+  
+      describe ', method #batch_items' do
+        let(:batch_items) { subject.batch_items }
+  
+        it ', returns BatchItem instances with full Contribution data' do
+            
+        end
+  
+        it ', returns BatchItem instances with partial Contribution data' do
+        end
+  
+        context ', when input CSV contains rows with emtpy cells for Contributor' do
+  
+  
+          let(:contribution_data) { subject.batch_items.map { |batch_item|
+            JSON.parse(batch_item.source_data)['Contribution']
+          }}
+  
+          it ', does not instantiate a BatchItem when Contribution data is empty' do
+            expect(batch_items.count).to eq (csv_rows.count - empty_csv_rows.count)
+          end
         end
       end
     end
