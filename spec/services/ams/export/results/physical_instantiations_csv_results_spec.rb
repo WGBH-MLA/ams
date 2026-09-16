fe517cff38523c9ce8a205771c24da3405bf6997
Andrew Myers <afredmyers@gmail.com>
2026-09-16 16:17:32 -0400

    Fixes broken export specs by using Valkyrie models instead of Feodra models

--- spec/services/ams/export/results/physical_instantiations_csv_results_spec.rb
+++ spec/services/ams/export/results/physical_instantiations_csv_results_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe AMS::Export::Results::DigitalInstantiationsCSVResults do
+RSpec.describe AMS::Export::Results::PhysicalInstantiationsCSVResults do
   describe '#filepath' do
     let(:assets) do
       Array.new(rand(1..3)) do
         create(:asset_resource,
           members: Array.new(rand(1..3)) do
-            create(:digital_instantiation_resource)
+            create(:physical_instantiation_resource)
           end
         )
       end
     end
 
     let(:solr_docs) do
-      assets.map { |asset| asset.digital_instantiations }.flatten
+      assets.map { |asset| asset.physical_instantiations }.flatten
     end
 
     subject { described_class.new(solr_documents: solr_docs) }
 
-    let(:expected_header) { AMS::CsvExportExtension.fields_for('digital_instantiation') }
-    let(:expected_rows) { solr_docs.map { |solr_doc| solr_doc.csv_row_for('digital_instantiation') } }
+    let(:expected_header) { AMS::CsvExportExtension.fields_for('physical_instantiation') }
+    let(:expected_rows) { solr_docs.map { |solr_doc| solr_doc.csv_row_for('physical_instantiation') } }
 
-    it 'points to a file containing Asset CSV Results' do
+    it 'points to a file containing PhysicalInstantiation CSV Results' do
       csv_rows = CSV.parse(File.read(subject.filepath))
       expect(expected_header).to eq csv_rows.first
       expect(expected_rows).to eq csv_rows.slice(1..-1)
