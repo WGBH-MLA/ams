de112f27bbe156f305e83bb1fb7782973525131b
Andrew Myers <afredmyers@gmail.com>
2026-09-18 11:37:53 -0400

    More massive removal of vestigial Fedora related code

--- app/presenters/hyrax/contribution_presenter.rb
+++
@@ -1,7 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work Contribution`
-module Hyrax
-  class ContributionPresenter < Hyrax::WorkShowPresenter
-    delegate :contributor_role, :portrayal, :affiliation, :annotation, to: :solr_document
-  end
-end
